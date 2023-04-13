12adc5bf5f7bd73ca2b02da720285a7f3c5f0b1c
seth <getchoo@tuta.io>
2023-04-13 19:06:46 -0400

    chore(refactor): inherit more functions in modules

--- modules/home-manager/bat.nix
+++ modules/home-manager/bat.nix
@@ -4,30 +4,33 @@
   lib,
   ...
 }: let
+  inherit (builtins) readFile;
+  inherit (lib) mkEnableOption mkIf mkOption types;
+  inherit (pkgs) bat fetchFromGitHub;
   cfg = config.programs.bat.catppuccin;
 in {
   options.programs.bat.catppuccin = {
-    enable = lib.mkEnableOption "Catppuccin theme";
-    flavour = lib.mkOption {
-      type = lib.types.enum ["latte" "frappe" "macchiato" "mocha"];
+    enable = mkEnableOption "Catppuccin theme";
+    flavour = mkOption {
+      type = types.enum ["latte" "frappe" "macchiato" "mocha"];
       default = config.catppuccin.flavour;
       description = "Catppuccin flavour for bat";
     };
   };
 
   config = {
-    home.activation.batCache = "${pkgs.bat}/bin/bat cache --build";
+    home.activation.batCache = "${bat}/bin/bat cache --build";
 
-    programs.bat = lib.mkIf cfg.enable {
+    programs.bat = mkIf cfg.enable {
       config.theme = "Catppuccin-${cfg.flavour}";
-      themes."Catppuccin-${cfg.flavour}" = builtins.readFile (pkgs.fetchFromGitHub
+      themes."Catppuccin-${cfg.flavour}" = readFile (fetchFromGitHub
         {
           owner = "catppuccin";
           repo = "bat";
           rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
           sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
         }
-        + /Catppuccin-${cfg.flavour}.tmTheme);
+        + "/Catppuccin-${cfg.flavour}.tmTheme");
     };
   };
 }
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,11 +1,12 @@
 {lib, ...}: let
   inherit (import ../utils.nix {inherit lib;}) importModules;
+  inherit (lib) mkOption types;
 in {
   imports = importModules ./.;
 
   options.catppuccin = {
-    flavour = lib.mkOption {
-      type = lib.types.enum ["latte" "frappe" "macchiato" "mocha"];
+    flavour = mkOption {
+      type = types.enum ["latte" "frappe" "macchiato" "mocha"];
       default = "latte";
       description = "Global Catppuccin flavour";
     };
