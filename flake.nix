6c8259210c82dc6e9d158100545d9f092c906c8e
seth <getchoo@tuta.io>
2023-04-13 23:35:15 -0400

    feat: add flake-compat support

--- flake.nix
+++ flake.nix
@@ -1,6 +1,14 @@
 {
   description = "Soothing pastel theme for Nix";
-  outputs = { self }: {
+
+  inputs = {
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = _: {
     nixosModules.default = import ./nixos;
     homeManagerModules.default = import ./home-manager;
   };
