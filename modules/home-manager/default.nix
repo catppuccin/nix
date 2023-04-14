7477194c791193219f1e43a699de1dc9f7f30501
seth <getchoo@tuta.io>
2023-04-14 17:24:01 -0400

    feat(modules): add support for gtk

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -2,12 +2,18 @@
   imports = [
     ./bat.nix
     ./starship.nix
+    ./gtk.nix
   ];
   options.catppuccin = {
     flavour = lib.mkOption {
       type = lib.types.enum [ "latte" "frappe" "macchiato" "mocha" ];
       default = "latte";
       description = "Global Catppuccin flavour";
+    };
+    accent = lib.mkOption {
+      type = lib.types.enum [ "blue" "flamingo" "green" "lavender" "maroon" "mauve" "peach" "pink" "red" "rosewater" "sapphire" "sky" "teal" "yellow" ];
+      default = "teal";
+      description = "Global Catppuccin accent";
     };
   };
 }
