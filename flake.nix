62bd90ec5b1f92fc15ec80432614657442a44421
seth <getchoo@tuta.io>
2023-06-02 15:25:15 -0400

    fix(modules): vendor our own revision of nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,14 +1,15 @@
 {
   description = "Soothing pastel theme for Nix";
   inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = _: {
-    nixosModules.catppuccin = import ./modules/nixos;
-    homeManagerModules.catppuccin = import ./modules/home-manager;
+  outputs = { nixpkgs, ... }: {
+    nixosModules.catppuccin = import ./modules/nixos nixpkgs;
+    homeManagerModules.catppuccin = import ./modules/home-manager nixpkgs;
   };
 }
