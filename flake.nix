f2332cd872f3b6fcb0d9657d5de4585f28a58039
seth <getchoo@tuta.io>
2023-06-02 15:29:08 -0400

    chore: set formatter to nixpkgs-fmt

--- flake.nix
+++ flake.nix
@@ -8,8 +8,27 @@
     };
   };
 
-  outputs = { nixpkgs, ... }: {
-    nixosModules.catppuccin = import ./modules/nixos nixpkgs;
-    homeManagerModules.catppuccin = import ./modules/home-manager nixpkgs;
-  };
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+      forEachSystem = fn:
+        forAllSystems (system:
+          fn {
+            inherit system;
+            pkgs = nixpkgsFor.${system};
+          });
+    in
+    {
+      nixosModules.catppuccin = import ./modules/nixos nixpkgs;
+      homeManagerModules.catppuccin = import ./modules/home-manager nixpkgs;
+      formatter = forEachSystem ({ pkgs, ... }: pkgs.nixpkgs-fmt);
+    };
 }
