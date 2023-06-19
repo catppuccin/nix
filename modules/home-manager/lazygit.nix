5371bec7633c64d9d6f56fcd5d459c29de7d8b56
isabel <isabel@isabelroses.com>
2023-06-19 16:14:19 +0100

    feat: add lazygit

---
+++ modules/home-manager/lazygit.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, lib, ... }:
+let cfg = config.programs.lazygit.catppuccin;
+in {
+  options.programs.lazygit.catppuccin =
+    lib.ctp.mkCatppuccinOpt "lazygit" config;
+
+  config.programs.lazygit.settings = with builtins;
+    with lib;
+    with pkgs;
+    let
+      file = fetchFromGitHub
+        {
+          owner = "catppuccin";
+          repo = "lazygit";
+          rev = "f01edfd57fa2aa7cd69a92537a613bb3c91e65dd";
+          sha256 = "sha256-zjzDtXcGtUon4QbrZnlAPzngEyH56yy8TCyFv0rIbOA=";
+        } + "/catppuccin-${cfg.flavour}.yml";
+
+    in
+    mkIf cfg.enable (ctp.fromYaml pkgs file);
+}
