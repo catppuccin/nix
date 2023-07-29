a60d2276228066c597cfb8e6d40053281958ab59
seth <getchoo@tuta.io>
2023-07-29 08:40:13 -0400

    feat: add autogenerated docs (#39)
    * feat: add auto generated docs
    
    * docs: init options docs

---
+++ docs/nixos-options.md
@@ -0,0 +1,50 @@
+## boot\.loader\.grub\.catppuccin\.enable
+
+Whether to enable Catppuccin theme.
+
+*Type:*
+boolean
+
+
+
+*Default:*
+` false `
+
+
+
+*Example:*
+` true `
+
+
+
+## boot\.loader\.grub\.catppuccin\.flavour
+
+Catppuccin flavour for grub
+
+
+
+*Type:*
+one of “latte”, “frappe”, “macchiato”, “mocha”
+
+
+
+*Default:*
+` "latte" `
+
+
+
+## catppuccin\.flavour
+
+Global Catppuccin flavour
+
+
+
+*Type:*
+one of “latte”, “frappe”, “macchiato”, “mocha”
+
+
+
+*Default:*
+` "latte" `
+
+
--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     };
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
       systems = [
         "x86_64-linux"
@@ -17,18 +17,51 @@
         "aarch64-darwin"
       ];
 
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-      forEachSystem = fn:
-        forAllSystems (system:
-          fn {
-            inherit system;
-            pkgs = nixpkgsFor.${system};
-          });
+      inherit (nixpkgs) lib;
+
+      forAllSystems = fn: lib.genAttrs systems (s: fn nixpkgsFor.${s});
+      nixpkgsFor = lib.genAttrs systems (system: import nixpkgs { inherit system; });
     in
     {
-      nixosModules.catppuccin = import ./modules/nixos nixpkgs;
+      formatter = forAllSystems (pkgs: pkgs.nixpkgs-fmt);
+
       homeManagerModules.catppuccin = import ./modules/home-manager nixpkgs;
-      formatter = forEachSystem ({ pkgs, ... }: pkgs.nixpkgs-fmt);
+
+      nixosModules.catppuccin = import ./modules/nixos nixpkgs;
+
+      packages = forAllSystems (pkgs:
+        let
+          mkEval = module: lib.evalModules {
+            modules = [
+              module
+              {
+                _module = {
+                  check = false;
+                  args.lib = import ./modules/lib/mkExtLib.nix lib;
+                };
+              }
+            ];
+          };
+
+          mkDoc = name: options:
+            let
+              doc = pkgs.nixosOptionsDoc {
+                options = lib.filterAttrs (n: _: n != "_module") options;
+                documentType = "none";
+                revision = if self ? rev then builtins.substring 0 7 self.rev else "dirty";
+              };
+            in
+            pkgs.runCommand "${name}-module-doc.md" { } ''
+              cat ${doc.optionsCommonMark} > $out
+            '';
+
+          hmEval = mkEval self.homeManagerModules.catppuccin;
+          nixosEval = mkEval self.nixosModules.catppuccin;
+        in
+        rec {
+          nixos-doc = mkDoc "nixos" nixosEval.options;
+          home-manager-doc = mkDoc "home-manager" hmEval.options;
+          default = home-manager-doc;
+        });
     };
 }
