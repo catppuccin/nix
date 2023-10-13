4ade2040125e692e90204a073a07a6c7f3063ded
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-10-13 22:51:17 +0800

    fix(home-manager/bat): use attrset for theme specification (#43)

--- modules/home-manager/bat.nix
+++ modules/home-manager/bat.nix
@@ -14,14 +14,17 @@ in
   config = {
     programs.bat = lib.mkIf enable {
       config.theme = "Catppuccin-${cfg.flavour}";
-      themes."Catppuccin-${cfg.flavour}" = builtins.readFile (pkgs.fetchFromGitHub
-        {
-          owner = "catppuccin";
-          repo = "bat";
-          rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
-          sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
-        }
-      + "/Catppuccin-${cfg.flavour}.tmTheme");
+      themes."Catppuccin-${cfg.flavour}" = {
+        src =
+          pkgs.fetchFromGitHub
+            {
+              owner = "catppuccin";
+              repo = "bat";
+              rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+              hash = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+            };
+        file = "Catppuccin-${cfg.flavour}.tmTheme";
+      };
     };
   };
 }
