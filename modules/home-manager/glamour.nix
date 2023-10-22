a97085d28b9e4b92f08dccf83087e5133dfbc079
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-10-23 00:48:48 +0800

    feat(modules/home-manager): add glamour (#44)

---
+++ modules/home-manager/glamour.nix
@@ -0,0 +1,31 @@
+{ config
+, pkgs
+, lib
+,
+}:
+let
+  cfg = config.programs.glamour.catppuccin;
+  enable = cfg.enable;
+
+  version = "1.0.0";
+
+  hashes = {
+    latte = "sha256-V0LsRStF1vL+Tz8G6VaKiwiY/ZIsSkMc+f1WJAITYXU=";
+    frappe = "sha256-YOatgYCJKuesVERHZVmF1xtzuLjyxCYstoWYqATq+NU=";
+    macchiato = "sha256-CeSJvhjzHVTtlqgQLKOrdLXtp2OQlMh24IaA1QQiQCk=";
+    mocha = "sha256-Tx2fQteL4wxhV+qHYZibakiYoEhS4HjyMO0yBcU/F6Q=";
+  };
+in
+{
+  options.programs.glamour.catppuccin =
+    lib.ctp.mkCatppuccinOpt "glamour" config;
+
+  config = {
+    home.sessionVariables = lib.mkIf enable {
+      GLAMOUR_STYLE = pkgs.fetchurl {
+        url = "https://github.com/catppuccin/glamour/releases/download/v${version}/${cfg.flavour}.json";
+        hash = hashes.${cfg.flavour};
+      };
+    };
+  };
+}
