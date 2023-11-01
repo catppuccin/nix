2072acaa8320a6529973830cfc7792b6cb09dc76
Isabel <71222764+isabelroses@users.noreply.github.com>
2023-11-01 22:45:25 +0000

    Update modules/home-manager/micro.nix
    Co-authored-by: seth <getchoo@tuta.io>

--- modules/home-manager/micro.nix
+++ modules/home-manager/micro.nix
@@ -4,11 +4,10 @@
 , ...
 }:
 let
-  inherit (lib) mkIf;
   cfg = config.programs.micro.catppuccin;
   enable = cfg.enable && config.programs.micro.enable;
 
-  themePath = "/catppuccin-${cfg.flavour}.micro";
+  themePath = "catppuccin-${cfg.flavour}.micro";
   theme =
     pkgs.fetchFromGitHub
       {
@@ -17,19 +16,20 @@ let
         rev = "ed8ef015f97c357575b5013e18042c9faa6c068a";
         sha256 = "/JwZ+5bLYjZWcV5vH22daLqVWbyJelqRyGa7V0b7EG8=";
       }
-    + "/src"
-    + themePath;
+    + "/src/${themePath}";
 in
 {
   options.programs.micro.catppuccin =
     lib.ctp.mkCatppuccinOpt "micro" config;
 
-  # xdg is required for this to work
-  config =
-    {
-      xdg.enable = mkIf enable (lib.mkForce true);
-    }
-    // (lib.mkIf enable {
-      xdg.configFile."micro/colorschemes${themePath}".source = theme;
-    });
+  config = lib.mkIf enable {
+    programs.micro.settings.colorscheme = lib.removeSuffix ".micro" themePath;
+
+    xdg = {
+      # xdg is required for this to work
+      enable = lib.mkForce true;
+      configFile."micro/colorschemes/${themePath}".source = theme;
+    };
+  };
+}
 }
