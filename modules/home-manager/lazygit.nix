cecf37c9524c5cbffffc9cf171d36f95bc75a115
seth <getchoo@tuta.io>
2023-11-02 13:23:32 -0400

    fix(home-manager): update paths from new lazygit theme revision

--- modules/home-manager/lazygit.nix
+++ modules/home-manager/lazygit.nix
@@ -5,16 +5,25 @@
 , ...
 }:
 let
+  inherit (lib) ctp;
   cfg = config.programs.lazygit.catppuccin;
   enable = cfg.enable && config.programs.lazygit.enable;
+
+  themePath = "/${cfg.flavour}/${cfg.accent}.yml";
 in
 {
   options.programs.lazygit.catppuccin =
-    lib.ctp.mkCatppuccinOpt "lazygit" config;
+    lib.ctp.mkCatppuccinOpt "lazygit" config // {
+      accent = ctp.mkAccentOpt "lazygit" config;
 
-  config.programs.lazygit.settings =
-    let
-      file = "${sources.lazygit}/themes/${cfg.flavour}.yml";
-    in
-    lib.mkIf enable (lib.ctp.fromYaml pkgs file);
+    };
+
+  config = lib.mkIf enable {
+
+    programs.lazygit.settings =
+      let
+        file = "${sources.lazygit}/themes/${themePath}";
+      in
+      lib.ctp.fromYaml pkgs file;
+  };
 }
