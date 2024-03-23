78fd4dd4e929c82cd7d26fe16e32e18e0560c7a9
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-03-23 18:52:27 -0400

    feat(home-manager): init fzf module

---
+++ modules/home-manager/fzf.nix
@@ -0,0 +1,35 @@
+{ config
+, lib
+, sources
+, ...
+}:
+let
+  cfg = config.programs.fzf.catppuccin;
+  enable = cfg.enable && config.programs.fzf.enable;
+  palette = (lib.importJSON "${sources.palette}/palette.json").${cfg.flavour}.colors;
+in
+{
+  options.programs.fzf.catppuccin =
+    lib.ctp.mkCatppuccinOpt "fzf";
+
+  config.programs.fzf.colors = lib.mkIf enable
+    # Manually populate with colors from catppuccin/fzf
+    # The ordering is meant to match the order of catppuccin/fzf to make comparison easier
+    (lib.attrsets.mapAttrs (_: color: palette.${color}.hex)
+      {
+        "bg+" = "surface0";
+        bg = "base";
+        spinner = "rosewater";
+        hl = "red";
+        fg = "text";
+        header = "red";
+        info = "mauve";
+        pointer = "rosewater";
+        marker = "rosewater";
+        "fg+" = "text";
+        prompt = "mauve";
+        "hl+" = "red";
+      }
+    );
+}
+
