56f3c604a80ca8efe37b7ffb7e09d384c464bfa7
Henri Sota <56848082+henrisota@users.noreply.github.com>
2024-04-11 06:35:22 +0200

    feat(home-manager): add support for rofi (#108)

---
+++ modules/home-manager/rofi.nix
@@ -0,0 +1,21 @@
+{ config
+, lib
+, sources
+, ...
+}:
+let
+  inherit (lib) ctp;
+  cfg = config.programs.rofi.catppuccin;
+  enable = cfg.enable && config.programs.rofi.enable;
+in
+{
+  options.programs.rofi.catppuccin =
+    lib.ctp.mkCatppuccinOpt "rofi";
+
+  config.programs.rofi = lib.mkIf enable {
+    theme = builtins.path {
+      name = "catppuccin-${cfg.flavour}.rasi";
+      path = "${sources.rofi}/basic/.local/share/rofi/themes/catppuccin-${cfg.flavour}.rasi";
+    };
+  };
+}
