060878986cce8e523557a783e21af3a5ebac8562
EnzioKam <enziokamhh@gmail.com>
2024-04-12 11:50:46 +0800

    fix(home-manager): link GTK 4.0 files

--- modules/home-manager/gtk.nix
+++ modules/home-manager/gtk.nix
@@ -62,4 +62,10 @@ in
         package = pkgs.catppuccin-cursors.${cfg.cursor.flavour + accentUpper};
       };
   };
+
+  config.xdg.configFile = lib.mkIf enable {
+    "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+    "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+    "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+  };
 }
