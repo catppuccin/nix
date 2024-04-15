2a5f99d84c15f67eabd0fe74937a02aa03807b0f
michaelBelsanti <quasigod-io@proton.me>
2024-04-15 00:27:19 -0400

    fix(home-manager): add file for yazi syntax highlighting

--- modules/home-manager/yazi.nix
+++ modules/home-manager/yazi.nix
@@ -13,6 +13,11 @@ in
     ctp.mkCatppuccinOpt "yazi";
 
   config = lib.mkIf enable {
+    assertions = [
+      (lib.ctp.assertXdgEnabled "yazi")
+    ];
+
     programs.yazi.theme = lib.importTOML "${sources.yazi}/themes/${cfg.flavour}.toml";
+    xdg.configFile."yazi/Catppuccin-${cfg.flavour}.tmTheme".source = "${sources.bat}/themes/Catppuccin ${ctp.mkUpper cfg.flavour}.tmTheme";
   };
 }
