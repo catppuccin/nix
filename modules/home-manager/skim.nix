11a575a83f29ea4d13b46d48c6b50ae2be601e5c
Lichthagel <hey@lichthagel.de>
2024-04-19 13:55:01 +0200

    feat(home-manager): add support for skim

---
+++ modules/home-manager/skim.nix
@@ -0,0 +1,19 @@
+{ config
+, lib
+, sources
+, ...
+}:
+let
+  cfg = config.programs.skim.catppuccin;
+  enable = cfg.enable && config.programs.skim.enable;
+  palette = (lib.importJSON "${sources.palette}/palette.json").${cfg.flavour}.colors;
+in
+{
+  options.programs.skim.catppuccin = lib.ctp.mkCatppuccinOpt "skim";
+
+  config.programs.skim = lib.mkIf enable {
+    defaultOptions = [
+      "--color=fg:${palette.text.hex},bg:${palette.base.hex},matched:${palette.surface0.hex},matched_bg:${palette.flamingo.hex},current:${palette.text.hex},current_bg:${palette.surface1.hex},current_match:${palette.base.hex},current_match_bg:${palette.rosewater.hex},spinner:${palette.green.hex},info:${palette.mauve.hex},prompt:${palette.blue.hex},cursor:${palette.red.hex},selected:${palette.maroon.hex},header:${palette.teal.hex},border:${palette.overlay0.hex}"
+    ];
+  };
+}
