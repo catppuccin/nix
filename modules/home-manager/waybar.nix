7bac8a8386be264122583840f545713e6cd31f12
Lichthagel <hey@lichthagel.de>
2024-04-20 10:38:18 +0200

    feat(home-manager/waybar): add prependImport & createLink options

--- modules/home-manager/waybar.nix
+++ modules/home-manager/waybar.nix
@@ -8,11 +8,33 @@ let
   enable = cfg.enable && config.programs.waybar.enable;
 in
 {
-  options.programs.waybar.catppuccin = lib.ctp.mkCatppuccinOpt "waybar";
+  options.programs.waybar.catppuccin = (lib.ctp.mkCatppuccinOpt "waybar") // {
+    styleFile = lib.mkOption {
+      type = lib.types.path;
+      default = "${sources.waybar}/themes/${cfg.flavour}.css";
+      description = "Path to CSS file containing color variable definitions";
+    };
 
-  config.programs.waybar = lib.mkIf enable {
-    style = lib.mkBefore ''
-      @import "${sources.waybar}/themes/${cfg.flavour}.css";
-    '';
+    prependImport = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Whether to prepend `@import \"$${styleFile}\";` when `programs.waybar.style` is set to a string.";
+    };
+
+    createLink = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Whether to create a symlink `~/.config/waybar/catppuccin.css` pointing to `styleFile`.";
+    };
+  };
+
+  config = lib.mkIf enable {
+    programs.waybar.style = lib.mkIf cfg.prependImport (
+      lib.mkBefore ''
+        @import "${cfg.styleFile}";
+      ''
+    );
+
+    xdg.configFile."waybar/catppuccin.css" = lib.mkIf cfg.createLink { source = cfg.styleFile; };
   };
 }
