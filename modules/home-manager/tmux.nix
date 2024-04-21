e279e72a6e044ed7ff9bd76404040294632e0a3c
vdbewout <vdbewout@gmail.com>
2024-04-21 16:31:18 +0200

    feat(tmux): add support for extraConfig
    Add the option extraConfig this allows users to configure the plugin

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -5,6 +5,7 @@
 , ...
 }:
 let
+  inherit (lib) ctp mkOption types concatStrings;
   cfg = config.programs.tmux.catppuccin;
   enable = cfg.enable && config.programs.tmux.enable;
 
@@ -18,12 +19,27 @@ let
 in
 {
   options.programs.tmux.catppuccin =
-    lib.ctp.mkCatppuccinOpt "tmux";
+    ctp.mkCatppuccinOpt "tmux"
+    // {
+      extraConfig = mkOption {
+        type = types.lines;
+        description = "Additional configuration for the catppuccin plugin.";
+        default = "";
+        example = ''
+          set -g @catppuccin_status_modules_right "application session user host date_time"
+        '';
+      };
+    };
 
   config.programs.tmux.plugins = lib.mkIf enable [
     {
       inherit plugin;
-      extraConfig = "set -g @catppuccin_flavour '${cfg.flavour}'";
+      extraConfig = concatStrings [
+        ''
+          set -g @catppuccin_flavour '${cfg.flavour}'
+        ''
+        cfg.extraConfig
+      ];
     }
   ];
 }
