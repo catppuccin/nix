61adcefb4956dbd62e8abcbeede2f623062976c2
Lichthagel <hey@lichthagel.de>
2024-04-23 23:28:59 +0200

    fix(home-manager/waybar): inline styleFile

--- modules/home-manager/waybar.nix
+++ modules/home-manager/waybar.nix
@@ -1,7 +1,8 @@
-{ config
-, lib
-, sources
-, ...
+{
+  config,
+  lib,
+  sources,
+  ...
 }:
 let
   cfg = config.programs.waybar.catppuccin;
@@ -9,12 +10,6 @@ let
 in
 {
   options.programs.waybar.catppuccin = (lib.ctp.mkCatppuccinOpt "waybar") // {
-    styleFile = lib.mkOption {
-      type = lib.types.path;
-      default = "${sources.waybar}/themes/${cfg.flavour}.css";
-      description = "Path to CSS file containing color variable definitions";
-    };
-
     prependImport = lib.mkOption {
       type = lib.types.bool;
       default = false;
@@ -28,13 +23,18 @@ in
     };
   };
 
-  config = lib.mkIf enable {
-    programs.waybar.style = lib.mkIf cfg.prependImport (
-      lib.mkBefore ''
-        @import "${cfg.styleFile}";
-      ''
-    );
+  config = lib.mkIf enable (
+    let
+      styleFile = "${sources.waybar}/themes/${cfg.flavour}.css";
+    in
+    {
+      programs.waybar.style = lib.mkIf cfg.prependImport (
+        lib.mkBefore ''
+          @import "${styleFile}";
+        ''
+      );
 
-    xdg.configFile."waybar/catppuccin.css" = lib.mkIf cfg.createLink { source = cfg.styleFile; };
-  };
+      xdg.configFile."waybar/catppuccin.css" = lib.mkIf cfg.createLink { source = styleFile; };
+    }
+  );
 }
