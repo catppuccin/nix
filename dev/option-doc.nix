8179a45f64c7448185eddcacaa81dce080cc45c2
seth <getchoo@tuta.io>
2024-05-21 01:27:57 -0600

    fix(modules): shorten defaultText for `catppuccin.sources` (#185)
    * fix: set nixos release in mkOptionDoc
    
    * fix(modules): shorten defaultText for `catppuccin.sources`

--- dev/option-doc.nix
+++ dev/option-doc.nix
@@ -7,11 +7,21 @@
    }:
 let
   eval = lib.evalModules {
-    modules = modules ++ [{ _module.check = false; }];
+    modules = modules ++ [{
+      options.system.nixos.release = lib.mkOption {
+        type = lib.types.str;
+        default = lib.trivial.release;
+        readOnly = true;
+      };
+
+      config = {
+        _module.check = false;
+      };
+    }];
   };
 
   doc = nixosOptionsDoc {
-    options = lib.filterAttrs (n: _: n != "_module") eval.options;
+    options = lib.filterAttrs (n: lib.const (!(lib.elem n [ "_module" "system" ]))) eval.options;
     documentType = "none";
     revision = version;
   };
