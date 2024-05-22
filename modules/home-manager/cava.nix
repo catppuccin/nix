cae2f757d1dda9fd84ad0fd97fd847f8fcbcb251
Adrien Cosson <ad.cosson@gmail.com>
2024-05-22 21:54:12 +0200

    feat: cava: add support for themes with transparent background

--- modules/home-manager/cava.nix
+++ modules/home-manager/cava.nix
@@ -3,11 +3,14 @@ let
   inherit (config.catppuccin) sources;
   cfg = config.programs.cava.catppuccin;
   enable = cfg.enable && config.programs.cava.enable;
+  flavor = "${cfg.flavor}" + lib.optionalString cfg.transparent "-transparent";
 in
 {
-  options.programs.cava.catppuccin = lib.ctp.mkCatppuccinOpt "cava";
+  options.programs.cava.catppuccin = lib.ctp.mkCatppuccinOpt "cava" // {
+    transparent = lib.mkEnableOption "transparent version of flavor";
+  };
 
   config.programs.cava = lib.mkIf enable {
-    settings = lib.ctp.fromINIRaw (sources.cava + "/themes/${cfg.flavor}.cava");
+    settings = lib.ctp.fromINIRaw (sources.cava + "/themes/${flavor}.cava");
   };
 }
