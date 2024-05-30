a089b917bc7b9083a479b7cc2f6331e7b897a932
seth <getchoo@tuta.io>
2024-05-30 08:06:29 -0400

    chore(modules)!: bump minimum supported release to 24.05

--- modules/home-manager/tofi.nix
+++ modules/home-manager/tofi.nix
@@ -3,15 +3,9 @@ let
   inherit (config.catppuccin) sources;
   cfg = config.programs.tofi.catppuccin;
   enable = cfg.enable && config.programs.tofi.enable;
-
-  # `programs.tofi` was added in 24.05 and not backported
-  # TODO: remove when 24.05 is stable
-  minVersion = "24.05";
 in
 {
-  options.programs.tofi = lib.ctp.mkVersionedOpts minVersion {
-    catppuccin = lib.ctp.mkCatppuccinOpt "tofi";
-  };
+  options.programs.tofi.catppuccin = lib.ctp.mkCatppuccinOpt "tofi";
 
   config.programs.tofi = lib.mkIf enable {
     settings = lib.ctp.fromINI (sources.tofi + "/catppuccin-${cfg.flavor}");
--- modules/nixos/sddm.nix
+++ modules/nixos/sddm.nix
@@ -10,45 +10,35 @@ let
     ctp
     types
     mkOption
-    versionAtLeast
     ;
   cfg = config.services.displayManager.sddm.catppuccin;
-  enable =
-    versionAtLeast ctp.getModuleRelease minVersion
-    && cfg.enable
-    && config.services.displayManager.sddm.enable;
-
-  # versions >= 24.05 renamed `services.xserver.displayManager` to `services.displayManager`
-  # TODO: remove when 24.05 is stable
-  minVersion = "24.05";
+  enable = cfg.enable && config.services.displayManager.sddm.enable;
 in
 
 {
-  options.services.displayManager = ctp.mkVersionedOpts minVersion {
-    sddm.catppuccin = ctp.mkCatppuccinOpt "sddm" // {
-      font = mkOption {
-        type = types.str;
-        default = "Noto Sans";
-        description = "Font to use for the login screen";
-      };
+  options.services.displayManager.sddm.catppuccin = ctp.mkCatppuccinOpt "sddm" // {
+    font = mkOption {
+      type = types.str;
+      default = "Noto Sans";
+      description = "Font to use for the login screen";
+    };
 
-      fontSize = mkOption {
-        type = types.str;
-        default = "9";
-        description = "Font size to use for the login screen";
-      };
+    fontSize = mkOption {
+      type = types.str;
+      default = "9";
+      description = "Font size to use for the login screen";
+    };
 
-      background = mkOption {
-        type = with types; (either path str);
-        default = "";
-        description = "Background image to use for the login screen";
-      };
+    background = mkOption {
+      type = with types; (either path str);
+      default = "";
+      description = "Background image to use for the login screen";
+    };
 
-      loginBackground = mkOption {
-        type = types.bool;
-        default = true;
-        description = "Add an additional background layer to the login panel";
-      };
+    loginBackground = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Add an additional background layer to the login panel";
     };
   };
 
