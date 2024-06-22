b6c854508d8c03f3ff06bf658d12b0ae8052d7a5
Isabel <isabel@isabelroses.com>
2024-06-22 13:31:23 +0100

    fix(nixos): use the qt 6 version of sddm (#230)
    * fix(nixos): use the qt 6 version of sddm
    
    the upstream port has moved to qt 6, while nixos' default is still qt 5
    
    * chore(nixos): use the qt6 version of sddm in tests
    
    ---------
    
    Co-authored-by: seth <getchoo@tuta.io>

--- modules/nixos/sddm.nix
+++ modules/nixos/sddm.nix
@@ -40,10 +40,32 @@ in
       default = true;
       description = "Add an additional background layer to the login panel";
     };
+
+    assertQt6Sddm =
+      lib.mkEnableOption ''
+        checking if `services.displayManager.sddm.package` is the Qt 6 version.
+
+        This is to ensure the theme is applied properly, but may have false positives in the case of overridden packages for example
+      ''
+      // {
+        default = true;
+      };
   };
 
   config = mkIf enable {
+    assertions = lib.optional cfg.assertQt6Sddm {
+      assertion = config.services.displayManager.sddm.package == pkgs.kdePackages.sddm;
+      message = ''
+        Only the Qt 6 version of SDDM is supported by this port!
+
+        In most cases this can be resolved by setting `services.displayManager.sddm.package`
+        to `pkgs.kdePackages.sddm`. If you know what you're doing and wish to disable this check,
+        please set `services.displayManager.sddm.catppuccin.assertQt6Sddm` to `false`
+      '';
+    };
+
     services.displayManager.sddm.theme = "catppuccin-${cfg.flavor}";
+
     environment.systemPackages = [
       (pkgs.catppuccin-sddm.override {
         inherit (cfg)
