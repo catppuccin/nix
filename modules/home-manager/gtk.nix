98efe771979681183132793fc13c44e691ba6335
Isabel <isabel@isabelroses.com>
2024-07-02 09:44:39 +0100

    Revert "fix(home-manager/gtk): replace `normal` tweak with `default` (#261)"
    This reverts commit 92e2d7ae880cd77677be66af2e77b2bc6b74f4fa.

--- modules/home-manager/gtk.nix
+++ modules/home-manager/gtk.nix
@@ -121,9 +121,7 @@ in
     (mkIf enable {
       gtk.theme =
         let
-          gtkTweaks = concatStringsSep "," (
-            map (tweak: { normal = "default"; }.${tweak} or tweak) cfg.tweaks
-          );
+          gtkTweaks = concatStringsSep "," cfg.tweaks;
         in
         {
           name = "catppuccin-${cfg.flavor}-${cfg.accent}-${cfg.size}+${gtkTweaks}";
