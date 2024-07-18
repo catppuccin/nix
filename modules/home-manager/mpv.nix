afc4170fb903e30dd8c4463a982f8951f0e61bb8
oli <benmoreosm@gmail.com>
2024-07-18 13:26:45 +0100

    feat(home-manager/mpv): add uosc

--- modules/home-manager/mpv.nix
+++ modules/home-manager/mpv.nix
@@ -1,18 +1,23 @@
 { config, lib, ... }:
 let
   inherit (config.catppuccin) sources;
+  inherit (lib) ctp mkIf;
   cfg = config.programs.mpv.catppuccin;
   enable = cfg.enable && config.programs.mpv.enable;
   themeDir = sources.mpv + "/themes/${cfg.flavor}/${cfg.accent}";
+  uoscDir = sources.mpv + "/uosc/themes/${cfg.flavor}/${cfg.accent}";
 in
 {
-  options.programs.mpv.catppuccin = lib.ctp.mkCatppuccinOpt { name = "mpv"; } // {
-    accent = lib.ctp.mkAccentOpt "mpv";
+  options.programs.mpv.catppuccin = ctp.mkCatppuccinOpt { name = "mpv"; } // {
+    accent = ctp.mkAccentOpt "mpv";
   };
 
   # Note that the theme is defined across multiple files
-  config.programs.mpv = lib.mkIf enable {
-    config = lib.ctp.fromINI (themeDir + "/mpv.conf");
-    scriptOpts.stats = lib.ctp.fromINI (themeDir + "/script-opts/stats.conf");
+  config.programs.mpv = mkIf enable {
+    config = ctp.fromINI (themeDir + "/mpv.conf");
+    scriptOpts = {
+      stats = ctp.fromINI (themeDir + "/script-opts/stats.conf");
+      uosc = ctp.fromINI (uoscDir + "/script-opts/uosc.conf");
+    };
   };
 }
