762fb5714d3cb834f367c9e9a525dfbdf8c36259
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-07-29 14:41:14 -0400

    fix(home-manager/zathura): avoid IFD

--- modules/home-manager/zathura.nix
+++ modules/home-manager/zathura.nix
@@ -1,23 +1,13 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}:
+{ config, lib, ... }:
 let
   inherit (config.catppuccin) sources;
   cfg = config.programs.zathura.catppuccin;
   enable = cfg.enable && config.programs.zathura.enable;
-  themeFile = sources.zathura + "/src/catppuccin-${cfg.flavor}";
 in
 {
   options.programs.zathura.catppuccin = lib.ctp.mkCatppuccinOpt { name = "zathura"; };
 
-  config.programs.zathura.options = lib.mkIf enable (
-    lib.ctp.fromINI (
-      pkgs.runCommand "catppuccin-zathura-theme" { } ''
-        ${pkgs.gawk}/bin/awk '/.+/ { printf "%s=%s\n", $2, $3 }' ${themeFile} > $out
-      ''
-    )
-  );
+  config.programs.zathura.extraConfig = lib.mkIf enable ''
+    include ${sources.zathura + "/src/catppuccin-${cfg.flavor}"}
+  '';
 }
