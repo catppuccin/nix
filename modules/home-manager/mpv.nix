513f9b487736efe254e85592eae43fbf0bf20963
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-08-06 16:08:52 -0400

    fix(home-manager/mpv): avoid ifd

--- modules/home-manager/mpv.nix
+++ modules/home-manager/mpv.nix
@@ -4,20 +4,13 @@ let
   inherit (lib) ctp mkIf;
   cfg = config.programs.mpv.catppuccin;
   enable = cfg.enable && config.programs.mpv.enable;
-  themeDir = sources.mpv + "/themes/${cfg.flavor}/${cfg.accent}";
-  uoscDir = sources.mpv + "/uosc/themes/${cfg.flavor}/${cfg.accent}";
 in
 {
   options.programs.mpv.catppuccin = ctp.mkCatppuccinOpt { name = "mpv"; } // {
     accent = ctp.mkAccentOpt "mpv";
   };
 
-  # Note that the theme is defined across multiple files
   config.programs.mpv = mkIf enable {
-    config = ctp.fromINI (themeDir + "/mpv.conf");
-    scriptOpts = {
-      stats = ctp.fromINI (themeDir + "/script-opts/stats.conf");
-      uosc = ctp.fromINI (uoscDir + "/script-opts/uosc.conf");
-    };
+    config.include = sources.mpv + "/themes/${cfg.flavor}/${cfg.accent}.conf";
   };
 }
