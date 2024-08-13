8f752151e505fff008c6ee252bf83a16d922082d
alejandro-angulo <iam@alejandr0angul0.dev>
2024-08-12 17:29:48 -0700

    fix(home-manager/k9s): k9s on darwin with xdg disabled
    k9s expects configuration files on OS X to live in
    `~/Library/Application Support/k9s` instead of `~/.config.k9s`. This
    commit handles the case where the module is built for a darwin system
    with xdg disabled.

--- modules/home-manager/k9s.nix
+++ modules/home-manager/k9s.nix
@@ -1,22 +1,32 @@
-{ config, lib, ... }:
-let
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
   inherit (config.catppuccin) sources;
+  inherit (pkgs.stdenv.hostPlatform) isDarwin;
 
   cfg = config.programs.k9s.catppuccin;
   enable = cfg.enable && config.programs.k9s.enable;
 
   themeName = "catppuccin-${cfg.flavor}" + lib.optionalString cfg.transparent "-transparent";
   themeFile = "${themeName}.yaml";
-  themePath = "/skins/${themeFile}";
+  configDirectory =
+    if (isDarwin && !config.xdg.enable)
+    then "/Library/Application Support/k9s"
+    else "k9s";
+  themePath = "${configDirectory}/skins/${themeFile}";
   theme = sources.k9s + "/dist/${themeFile}";
-in
-{
-  options.programs.k9s.catppuccin = lib.ctp.mkCatppuccinOpt { name = "k9s"; } // {
-    transparent = lib.mkEnableOption "transparent version of flavor";
-  };
+in {
+  options.programs.k9s.catppuccin =
+    lib.ctp.mkCatppuccinOpt {name = "k9s";}
+    // {
+      transparent = lib.mkEnableOption "transparent version of flavor";
+    };
 
   config = lib.mkIf enable {
-    xdg.configFile."k9s${themePath}".source = theme;
+    xdg.configFile."${themePath}".source = theme;
 
     programs.k9s.settings.k9s.ui.skin = themeName;
   };
