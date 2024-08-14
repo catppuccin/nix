41f7b71b6c94a81b09bdfc30cf535eab8c922590
alejandro-angulo <iam@alejandr0angul0.dev>
2024-08-13 21:13:09 -0700

    fix(home-manager/lazygit): support darwin without XDG
    Lazygit would fail to load becase the wrong path was being generated to
    the configuration file on darwin systems when XDG wasn't enabled.

--- modules/home-manager/lazygit.nix
+++ modules/home-manager/lazygit.nix
@@ -1,17 +1,28 @@
-{ config, lib, ... }:
-let
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
   inherit (lib) ctp;
   inherit (config.catppuccin) sources;
+  inherit (pkgs.stdenv.hostPlatform) isDarwin;
+
   cfg = config.programs.lazygit.catppuccin;
   enable = cfg.enable && config.programs.lazygit.enable;
-in
-{
-  options.programs.lazygit.catppuccin = lib.ctp.mkCatppuccinOpt { name = "lazygit"; } // {
-    accent = ctp.mkAccentOpt "lazygit";
-  };
+  configDirectory =
+    if (isDarwin && !config.xdg.enable)
+    then "${config.home.homeDirectory}/Library/Application Support/lazygit"
+    else "${config.xdg.configHome}/lazygit";
+in {
+  options.programs.lazygit.catppuccin =
+    lib.ctp.mkCatppuccinOpt {name = "lazygit";}
+    // {
+      accent = ctp.mkAccentOpt "lazygit";
+    };
 
   config.home.sessionVariables = lib.mkIf enable {
     # Ensure that the default config file is still sourced
-    LG_CONFIG_FILE = "${sources.lazygit}/themes-mergable/${cfg.flavor}/${cfg.accent}.yml,${config.xdg.configHome}/lazygit/config.yml";
+    LG_CONFIG_FILE = "${sources.lazygit}/themes-mergable/${cfg.flavor}/${cfg.accent}.yml,${configDirectory}/config.yml";
   };
 }
