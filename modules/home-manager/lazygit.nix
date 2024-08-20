becaf701b2b56e765cc895a24bdb3f8cb45f0429
alejandro-angulo <iam@alejandr0angul0.dev>
2024-08-19 20:52:33 -0700

    Refactored

--- modules/home-manager/lazygit.nix
+++ modules/home-manager/lazygit.nix
@@ -3,26 +3,32 @@
   lib,
   pkgs,
   ...
-}: let
+}:
+let
   inherit (lib) ctp;
   inherit (config.catppuccin) sources;
-  inherit (pkgs.stdenv.hostPlatform) isDarwin;
 
   cfg = config.programs.lazygit.catppuccin;
   enable = cfg.enable && config.programs.lazygit.enable;
+
+  # NOTE: On MacOS specifically, k9s expects its configuration to be in
+  # `~/Library/Application Support` when not using XDG
+  enableXdgConfig = !pkgs.stdenv.hostPlatform.isDarwin || config.xdg.enable;
+
   configDirectory =
-    if (isDarwin && !config.xdg.enable)
-    then "${config.home.homeDirectory}/Library/Application Support/lazygit"
-    else "${config.xdg.configHome}/lazygit";
-in {
-  options.programs.lazygit.catppuccin =
-    lib.ctp.mkCatppuccinOpt {name = "lazygit";}
-    // {
-      accent = ctp.mkAccentOpt "lazygit";
-    };
+    if enableXdgConfig then
+      "${config.home.homeDirectory}/Library/Application Support"
+    else
+      "${config.xdg.configHome}";
+  configFile = "${configDirectory}/lazygit/config.yml";
+in
+{
+  options.programs.lazygit.catppuccin = lib.ctp.mkCatppuccinOpt { name = "lazygit"; } // {
+    accent = ctp.mkAccentOpt "lazygit";
+  };
 
   config.home.sessionVariables = lib.mkIf enable {
     # Ensure that the default config file is still sourced
-    LG_CONFIG_FILE = "${sources.lazygit}/themes-mergable/${cfg.flavor}/${cfg.accent}.yml,${configDirectory}/config.yml";
+    LG_CONFIG_FILE = "${sources.lazygit}/themes-mergable/${cfg.flavor}/${cfg.accent}.yml,${configFile}";
   };
 }
