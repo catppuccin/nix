a9437b0882a5cc64da87231e61e387abeb28d5ac
alejandro-angulo <iam@alejandr0angul0.dev>
2024-08-19 20:47:03 -0700

    Refactored
    - More closely matches the upstream module in home-manager
    - Formatted with nixfmt-rfc-style

--- modules/home-manager/k9s.nix
+++ modules/home-manager/k9s.nix
@@ -3,31 +3,32 @@
   lib,
   pkgs,
   ...
-}: let
+}:
+let
   inherit (config.catppuccin) sources;
-  inherit (pkgs.stdenv.hostPlatform) isDarwin;
 
   cfg = config.programs.k9s.catppuccin;
   enable = cfg.enable && config.programs.k9s.enable;
 
+  # NOTE: On MacOS specifically, k9s expects its configuration to be in
+  # `~/Library/Application Support` when not using XDG
+  enableXdgConfig = !pkgs.stdenv.hostPlatform.isDarwin || config.xdg.enable;
+
   themeName = "catppuccin-${cfg.flavor}" + lib.optionalString cfg.transparent "-transparent";
   themeFile = "${themeName}.yaml";
-  configDirectory =
-    if (isDarwin && !config.xdg.enable)
-    then "/Library/Application Support/k9s"
-    else "k9s";
-  themePath = "${configDirectory}/skins/${themeFile}";
+  themePath = "k9s/skins/${themeFile}";
   theme = sources.k9s + "/dist/${themeFile}";
-in {
-  options.programs.k9s.catppuccin =
-    lib.ctp.mkCatppuccinOpt {name = "k9s";}
-    // {
-      transparent = lib.mkEnableOption "transparent version of flavor";
-    };
-
-  config = lib.mkIf enable {
-    xdg.configFile."${themePath}".source = theme;
-
-    programs.k9s.settings.k9s.ui.skin = themeName;
+in
+{
+  options.programs.k9s.catppuccin = lib.ctp.mkCatppuccinOpt { name = "k9s"; } // {
+    transparent = lib.mkEnableOption "transparent version of flavor";
   };
+
+  config = lib.mkIf enable lib.mkMerge [
+    (lib.mkIf (!enableXdgConfig) {
+      home.file."Library/Application Support/${themePath}".source = theme;
+    })
+    (lib.mkIf enableXdgConfig { xdg.configFile.${themePath}.source = theme; })
+    { programs.k9s.settings.k9s.ui.skin = themeName; }
+  ];
 }
