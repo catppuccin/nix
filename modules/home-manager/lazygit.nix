8886a68edadb1d93c7101337f995ffce4b410ff2
Alejandro Angulo <5242883+alejandro-angulo@users.noreply.github.com>
2024-08-20 05:17:35 -0700

    fix(home-manager/lazygit): support darwin without XDG (#313)
    * fix(home-manager/lazygit): support darwin without XDG
    
    Lazygit would fail to load becase the wrong path was being generated to
    the configuration file on darwin systems when XDG wasn't enabled.
    
    * Refactored
    
    * fix(home-manager/lazygit): actually use xdg directory when it's enabled
    
    ---------
    
    Co-authored-by: seth <getchoo@tuta.io>

--- modules/home-manager/lazygit.nix
+++ modules/home-manager/lazygit.nix
@@ -1,9 +1,26 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   inherit (lib) ctp;
   inherit (config.catppuccin) sources;
+
   cfg = config.programs.lazygit.catppuccin;
   enable = cfg.enable && config.programs.lazygit.enable;
+
+  # NOTE: On MacOS specifically, k9s expects its configuration to be in
+  # `~/Library/Application Support` when not using XDG
+  enableXdgConfig = !pkgs.stdenv.hostPlatform.isDarwin || config.xdg.enable;
+
+  configDirectory =
+    if enableXdgConfig then
+      config.xdg.configHome
+    else
+      "${config.home.homeDirectory}/Library/Application Support";
+  configFile = "${configDirectory}/lazygit/config.yml";
 in
 {
   options.programs.lazygit.catppuccin = lib.ctp.mkCatppuccinOpt { name = "lazygit"; } // {
@@ -12,6 +29,6 @@ in
 
   config.home.sessionVariables = lib.mkIf enable {
     # Ensure that the default config file is still sourced
-    LG_CONFIG_FILE = "${sources.lazygit}/themes-mergable/${cfg.flavor}/${cfg.accent}.yml,${config.xdg.configHome}/lazygit/config.yml";
+    LG_CONFIG_FILE = "${sources.lazygit}/themes-mergable/${cfg.flavor}/${cfg.accent}.yml,${configFile}";
   };
 }
