6bec9a29f4726571f977b78189f405163380afb8
SchweGELBin <67663319+SchweGELBin@users.noreply.github.com>
2024-08-30 16:32:52 +0200

    fix: add enable option to fuzzel
    It only checked if catppuccin was enabled globally

--- modules/home-manager/fuzzel.nix
+++ modules/home-manager/fuzzel.nix
@@ -2,13 +2,14 @@
 let
   inherit (config.catppuccin) sources;
   cfg = config.programs.fuzzel.catppuccin;
+  enable = cfg.enable && config.programs.fuzzel.enable;
 in
 {
   options.programs.fuzzel.catppuccin = lib.ctp.mkCatppuccinOpt { name = "fuzzel"; } // {
     accent = lib.ctp.mkAccentOpt "fuzzel";
   };
 
-  config = lib.mkIf cfg.enable {
+  config = lib.mkIf enable {
     programs.fuzzel.settings.main.include = sources.fuzzel + "/themes/${cfg.flavor}/${cfg.accent}.ini";
   };
 }
