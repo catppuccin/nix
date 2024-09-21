966af28f2d5b7b5bd3f947d6bab5d7da3bf9fee4
SchweGELBin <67663319+SchweGELBin@users.noreply.github.com>
2024-09-21 22:43:11 +0200

    feat(home-manager/mako): add accent color support (#323)
    * feat(mako): add accent color support
    
    * fix(mako): updated theme path
    
    * chore(mako): update source
    
    * fix(mako): add accent option

--- modules/home-manager/mako.nix
+++ modules/home-manager/mako.nix
@@ -8,13 +8,15 @@ let
   inherit (config.catppuccin) sources;
   cfg = config.services.mako.catppuccin;
   enable = cfg.enable && config.services.mako.enable;
-  theme = lib.ctp.fromINI (sources.mako + "/themes/${cfg.flavor}");
+  theme = lib.ctp.fromINI (sources.mako + "/themes/catppuccin-${cfg.flavor}/catppuccin-${cfg.flavor}-${cfg.accent}");
 
   # Settings that need to be extracted and put in extraConfig
   extraConfigAttrs = lib.attrsets.getAttrs [ "urgency=high" ] theme;
 in
 {
-  options.services.mako.catppuccin = lib.ctp.mkCatppuccinOpt { name = "mako"; };
+  options.services.mako.catppuccin = lib.ctp.mkCatppuccinOpt { name = "mako"; } // {
+    accent = lib.ctp.mkAccentOpt "mako";
+  };
 
   # Will cause infinite recursion if config.services.mako is directly set as a whole
   config.services.mako = lib.mkIf enable {
