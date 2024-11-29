7546e269ebb95a6c1d26f2b8814ffd4512884717
seth <getchoo@tuta.io>
2024-11-29 02:26:44 -0500

    feat: init package set

--- flake.nix
+++ flake.nix
@@ -1,8 +1,48 @@
 {
   description = "Soothing pastel theme for Nix";
 
-  outputs = _: {
-    homeManagerModules.catppuccin = import ./modules/home-manager;
-    nixosModules.catppuccin = import ./modules/nixos;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
+
+  outputs =
+    { nixpkgs, self }:
+
+    let
+      inherit (nixpkgs) lib;
+      systems = lib.systems.flakeExposed;
+
+      # flake-utils pollyfill
+      forEachSystem =
+        fn:
+        lib.foldl' (
+          acc: system:
+          lib.recursiveUpdate acc (
+            lib.mapAttrs (lib.const (value: {
+              ${system} = value;
+            })) (fn system)
+          )
+        ) { } systems;
+    in
+
+    lib.mergeAttrsList [
+      (forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          packages =
+            let
+              catppuccinPackages = (import ./default.nix { inherit pkgs; }).packages;
+            in
+            catppuccinPackages // { default = pkgs.emptyFile; };
+        }
+      ))
+
+      {
+        homeManagerModules.catppuccin = import ./modules/home-manager;
+        nixosModules.catppuccin = import ./modules/nixos;
+      }
+    ];
 }
