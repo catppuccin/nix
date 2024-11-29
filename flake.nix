54b973748d2a18829c6045afa1646b919db89cb6
seth <getchoo@tuta.io>
2024-11-28 23:59:08 -0500

    refactor: merge subflake back into main flake
    this is annoying to maintain

--- flake.nix
+++ flake.nix
@@ -3,10 +3,45 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    /*
+      Inputs below this are optional and can be removed
+
+      ```
+      {
+        inputs.catppuccin = {
+          url = "github:catppuccin/nix";
+          inputs = {
+            nixpkgs-stable.follows = "";
+            home-manager.follows = "";
+            home-manager-stable.follows = "";
+          };
+        };
+      }
+      ```
+    */
+
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager-stable = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
+    };
   };
 
   outputs =
-    { nixpkgs, self }:
+    {
+      self,
+      nixpkgs,
+      nixpkgs-stable,
+      home-manager,
+      home-manager-stable,
+    }:
 
     let
       inherit (nixpkgs) lib;
@@ -45,17 +80,81 @@
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+          pkgsStable = nixpkgs-stable.legacyPackages.${system};
         in
         {
+          apps = {
+            serve = {
+              type = "app";
+              program = lib.getExe self.packages.${system}.site.serve;
+            };
+          };
+
+          checks =
+            let
+              inherit (pkgs.stdenv.hostPlatform) isDarwin isLinux;
+
+              callWith = pkgs: lib.flip pkgs.callPackage;
+              callUnstable = callWith pkgs { inherit home-manager; };
+              callStable = callWith pkgsStable { home-manager = home-manager-stable; };
+            in
+            lib.optionalAttrs isDarwin {
+              darwin-test-unstable = callUnstable ./tests/darwin.nix;
+              darwin-test-stable = callStable ./tests/darwin.nix;
+            }
+            // lib.optionalAttrs isLinux {
+              nixos-test-unstable = callUnstable ./tests/nixos.nix;
+              nixos-test-stable = callStable ./tests/nixos.nix;
+            };
+
           devShells.default = import ./shell.nix { inherit pkgs; };
 
           formatter = pkgs.nixfmt-rfc-style;
 
           packages =
             let
+              packages' = self.packages.${system};
+
               catppuccinPackages = (import ./default.nix { inherit pkgs; }).packages;
+
+              docVersion = self.shortRev or self.dirtyShortRev or "unknown";
+              mkOptionDoc = pkgs.callPackage ./docs/options-doc.nix { };
+              mkSite = pkgs.callPackage ./docs/mk-site.nix { };
             in
-            catppuccinPackages // { default = catppuccinPackages.whiskers; };
+            catppuccinPackages
+            // {
+              default = catppuccinPackages.whiskers;
+
+              nixos-doc = mkOptionDoc {
+                version = docVersion;
+                moduleRoot = ./modules/nixos;
+              };
+
+              home-manager-doc = mkOptionDoc {
+                version = docVersion;
+                moduleRoot = ./modules/home-manager;
+              };
+
+              site = mkSite rec {
+                pname = "catppuccin-nix-website";
+                version = docVersion;
+
+                src = lib.fileset.toSource {
+                  root = ./.;
+                  fileset = lib.fileset.unions [
+                    ./CHANGELOG.md
+                    ./docs/src
+                    ./docs/book.toml
+                    ./docs/theme
+                  ];
+                };
+
+                sourceRoot = "${src.name}/docs";
+
+                nixosDoc = packages'.nixos-doc;
+                homeManagerDoc = packages'.home-manager-doc;
+              };
+            };
         }
       ))
 
