c38ee81d85e630f31ccc9a05d19dcac60985abc7
seth <getchoo@tuta.io>
2024-11-28 23:59:08 -0500

    refactor: merge subflake back into main flake
    this is annoying to maintain
    
    • Added input 'catppuccin-v1_1':
        'https://api.flakehub.com/f/pinned/catppuccin/nix/1.1.1/0193bdc0-b045-7eed-bbec-95611a8ecdf5/source.tar.gz?narHash=sha256-pCWJgwo77KD7EJpwynwKrWPZ//dwypHq2TfdzZWqK68%3D' (2024-12-13)
    • Added input 'catppuccin-v1_2':
        'https://api.flakehub.com/f/pinned/catppuccin/nix/1.2.0/0193e5e0-33b7-7149-a362-bfe56b20f64e/source.tar.gz?narHash=sha256-Let3uJo4YDyfqbqaw66dpZxhJB2TrDyZWSFd5rpPLJA%3D' (2024-12-20)
    • Added input 'home-manager':
        'github:nix-community/home-manager/1395379a7a36e40f2a76e7b9936cc52950baa1be?narHash=sha256-OOfI0XhSJGHblfdNDhfnn8QnZxng63rWk9eeJ2tCbiI%3D' (2024-12-19)
    • Added input 'home-manager/nixpkgs':
        follows 'nixpkgs'
    • Added input 'home-manager-stable':
        'github:nix-community/home-manager/80b0fdf483c5d1cb75aaad909bd390d48673857f?narHash=sha256-vykpJ1xsdkv0j8WOVXrRFHUAdp9NXHpxdnn1F4pYgSw%3D' (2024-12-16)
    • Added input 'home-manager-stable/nixpkgs':
        follows 'nixpkgs-stable'
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/23e89b7da85c3640bbc2173fe04f4bd114342367?narHash=sha256-y/MEyuJ5oBWrWAic/14LaIr/u5E0wRVzyYsouYY3W6w%3D' (2024-11-19)
      → 'github:NixOS/nixpkgs/d3c42f187194c26d9f0309a8ecc469d6c878ce33?narHash=sha256-cHar1vqHOOyC7f1%2BtVycPoWTfKIaqkoe1Q6TnKzuti4%3D' (2024-12-17)
    • Added input 'nixpkgs-stable':
        'github:NixOS/nixpkgs/b47fd6fa00c6afca88b8ee46cfdb00e104f50bca?narHash=sha256-nbG9TijTMcfr%2Bau7ZVbKpAhMJzzE2nQBYmRvSdXUD8g%3D' (2024-12-19)
    • Added input 'nuscht-search':
        'github:NuschtOS/search/3051be7f403bff1d1d380e4612f0c70675b44fc9?narHash=sha256-Y47y%2BLesOCkJaLvj%2BdI/Oa6FAKj/T9sKVKDXLNsViPw%3D' (2024-12-09)
    • Added input 'nuscht-search/flake-utils':
        'github:numtide/flake-utils/11707dc2f618dd54ca8739b309ec4fc024de578b?narHash=sha256-l0KFg5HjrsfsO/JpG%2Br7fRrqm12kzFHyUHqHCVpMMbI%3D' (2024-11-13)
    • Added input 'nuscht-search/flake-utils/systems':
        'github:nix-systems/default/da67096a3b9bf56a91d16901293e51ba5b49a27e?narHash=sha256-Vy1rq5AaRuLzOxct8nz4T6wlgyUR7zLU309k9mBC768%3D' (2023-04-09)
    • Added input 'nuscht-search/ixx':
        'github:NuschtOS/ixx/9fd01aad037f345350eab2cd45e1946cc66da4eb?narHash=sha256-EiOq8jF4Z/zQe0QYVc3%2BqSKxRK//CFHMB84aYrYGwEs%3D' (2024-10-26)
    • Added input 'nuscht-search/ixx/flake-utils':
        follows 'nuscht-search/flake-utils'
    • Added input 'nuscht-search/ixx/nixpkgs':
        follows 'nuscht-search/nixpkgs'
    • Added input 'nuscht-search/nixpkgs':
        follows 'nixpkgs'

--- flake.nix
+++ flake.nix
@@ -3,18 +3,79 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    /*
+      Inputs below this are optional and can be removed
+
+      ```
+      {
+        inputs.catppuccin = {
+          url = "github:catppuccin/nix";
+          inputs = {
+            nixpkgs-stable.follows = "";
+            home-manager.follows = "";
+            home-manager-stable.follows = "";
+            nuscht-search.follows = "";
+            catppuccin-v1_1.follows = "";
+            catppuccin-v1_2.follows = "";
+          };
+        };
+      }
+      ```
+    */
+
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager-stable = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
+    };
+
+    nuscht-search = {
+      url = "github:NuschtOS/search";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Track some of our minor releases to index in our search
+
+    catppuccin-v1_1 = {
+      url = "https://flakehub.com/f/catppuccin/nix/1.1.*.tar.gz";
+    };
+
+    catppuccin-v1_2 = {
+      url = "https://flakehub.com/f/catppuccin/nix/1.2.*.tar.gz";
+    };
   };
 
   outputs =
-    { nixpkgs, self }:
+    {
+      self,
+      nixpkgs,
+      ...
+    }@inputs:
 
     let
       inherit (nixpkgs) lib;
+
+      # Systems for public outputs
       systems = lib.systems.flakeExposed;
+
+      # Systems for development relatedo otuputs
+      devSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
       # flake-utils pollyfill
       forEachSystem =
-        fn:
+        systems: fn:
         lib.foldl' (
           acc: system:
           lib.recursiveUpdate acc (
@@ -23,6 +84,16 @@
             })) (fn system)
           )
         ) { } systems;
+
+      forEachDefaultSystem = forEachSystem systems;
+      forEachDevSystem = forEachSystem devSystems;
+
+      # Versions of the modules we want to index in our search
+      searchVersions = {
+        "v1.1" = inputs.catppuccin-v1_1;
+        "v1.2" = inputs.catppuccin-v1_2;
+        "rolling" = self;
+      };
 
       mkModule =
         {
@@ -44,21 +115,18 @@
 
     mergeAttrs [
       # Public outputs
-      (forEachSystem (
+      (forEachDefaultSystem (
         system:
+
         let
           pkgs = nixpkgs.legacyPackages.${system};
+          catppuccinPackages = (import ./default.nix { inherit pkgs; }).packages;
         in
+
         {
-          devShells.default = import ./shell.nix { inherit pkgs; };
-
-          formatter = pkgs.nixfmt-rfc-style;
-
-          packages =
-            let
-              catppuccinPackages = (import ./default.nix { inherit pkgs; }).packages;
-            in
-            catppuccinPackages // { default = catppuccinPackages.whiskers; };
+          packages = catppuccinPackages // {
+            default = catppuccinPackages.whiskers;
+          };
         }
       ))
 
@@ -73,5 +141,58 @@
           file = ./modules/nixos;
         };
       }
+
+      # Development outputs
+      (forEachDevSystem (
+        system:
+
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          pkgsStable = inputs.nixpkgs-stable.legacyPackages.${system};
+        in
+
+        {
+          apps = {
+            serve = {
+              type = "app";
+              program = lib.getExe self.packages.${system}.site.serve;
+            };
+          };
+
+          checks =
+
+            let
+              kernelName = pkgs.stdenv.hostPlatform.parsed.kernel.name;
+
+              callWith = pkgs: lib.flip pkgs.callPackage;
+              callUnstable = callWith pkgs { inherit (inputs) home-manager; };
+              callStable = callWith pkgsStable { home-manager = inputs.home-manager-stable; };
+            in
+
+            {
+              darwin = {
+                test-unstable = callUnstable ./tests/darwin.nix;
+                test-stable = callStable ./tests/darwin.nix;
+              };
+
+              linux = {
+                test-unstable = callUnstable ./tests/nixos.nix;
+                test-stable = callStable ./tests/nixos.nix;
+              };
+            }
+            .${kernelName} or { };
+
+          devShells.default = import ./shell.nix { inherit pkgs; };
+
+          formatter = pkgs.nixfmt-rfc-style;
+
+          packages = {
+            site = pkgs.callPackage ./docs/package.nix {
+              inherit inputs searchVersions;
+              nuscht-search = inputs.nuscht-search.packages.${system};
+            };
+          };
+        }
+      ))
     ];
 }
