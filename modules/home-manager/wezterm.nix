692cd0e0a3f4a185916127026db4e29a366aeded
taranarmo <taranarmo@gmail.com>
2025-01-04 14:24:02 +0100

    Apply suggestions from code review
    Co-authored-by: Isabel <isabel@isabelroses.com>

--- modules/home-manager/wezterm.nix
+++ modules/home-manager/wezterm.nix
@@ -4,28 +4,27 @@
 let
   inherit (config.catppuccin) sources;
   cfg = config.catppuccin.wezterm;
-  applyWeztermTheme = config.catppuccin.wezterm.apply or false;
 in
 {
-  options.catppuccin.wezterm = catppuccinLib.mkCatppuccinOption { name = "wezterm"; };
-
-  options.catppuccin.wezterm.apply = lib.mkOption {
-    type = lib.types.bool;
-    default = false;
-    description = "Apply Catppuccin theme to WezTerm.";
+  options.catppuccin.wezterm = catppuccinLib.mkCatppuccinOption { name = "wezterm"; } // {
+    apply = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Apply Catppuccin theme to WezTerm.";
+    };
   };
 
-  config = lib.mkIf (cfg.enable && applyWeztermTheme) {
+  config = lib.mkIf cfg.enable {
     programs.wezterm = {
       colorSchemes."catppuccin-${cfg.flavor}" = lib.importTOML "${sources.wezterm}/dist/catppuccin-${cfg.flavor}.toml";
-      extraConfig = lib.mkBefore ''
+      extraConfig = lib.mkIf cfg.apply (lib.mkBefore ''
         local config = {}
         if wezterm.config_builder then
           config = wezterm.config_builder()
         end
 
         dofile("${sources.wezterm}/plugin/init.lua").apply_to_config(config)
-      '';
+      '');
     };
   };
 }
