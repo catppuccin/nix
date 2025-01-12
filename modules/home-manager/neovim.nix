14c63cdede2faf0c9fa8b30bada57b3121b37620
Luna Heyman <luna.heyman@proton.me>
2025-01-12 18:28:34 +0100

    refactor(home-manager/neovim): use lib.generators.toLua function for extraConfig

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -1,36 +1,14 @@
 { catppuccinLib }:
-{
-  config,
-  lib,
-  ...
-}:
-
+{ config, lib, ... }:
 let
   cfg = config.catppuccin.nvim;
+  pluginSettings = lib.generators.toLua { } (defaultConfig // cfg.extraConfig);
 
-  toLuaValue =
-    value:
-    if builtins.isAttrs value then
-      toLuaTable value
-    else if lib.isString value || lib.isPath value then
-      ''"${toString value}"''
-    else if lib.isList value then
-      "{${lib.strings.concatStringsSep "," (map toLuaValue value)}}"
-    else if lib.isBool value then
-      if value == true then "true" else "false"
-    else
-      toString value;
-
-  toLuaTable =
-    attrset:
-    let
-      values = lib.strings.concatStringsSep "," (
-        lib.attrsets.mapAttrsToList (name: value: "${name} = ${toLuaValue value}") attrset
-      );
-    in
-    "{${values}}";
+  defaultConfig = {
+    compile_path = lib.generators.mkLuaInline "compile_path";
+    flavour = cfg.flavor;
+  };
 in
-
 {
   options.catppuccin.nvim = catppuccinLib.mkCatppuccinOption { name = "neovim"; } // {
     extraConfig = lib.mkOption {
@@ -60,18 +38,7 @@ in
               vim.fn.mkdir(compile_path, "p")
               vim.opt.runtimepath:append(compile_path)
 
-              local catppuccin_custom_plugin_settings = ${toLuaTable cfg.extraConfig}
-
-              local catppuccin_plugin_settings = {
-                compile_path = compile_path,
-                flavour = "${cfg.flavor}",
-              }
-
-              for key, value in pairs(catppuccin_custom_plugin_settings) do
-                catppuccin_plugin_settings[key] = value
-              end
-
-              require("catppuccin").setup(catppuccin_plugin_settings)
+              require("catppuccin").setup(${pluginSettings})
 
               vim.api.nvim_command("colorscheme catppuccin")
             EOF
