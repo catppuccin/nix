525e42b9dde8b325a5c7a475f004c567cef79b43
Luna Heyman <luna.heyman@proton.me>
2025-01-12 17:07:39 +0100

    feat(home-manager/neovim): add option for custom settings

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -7,10 +7,38 @@
 
 let
   cfg = config.catppuccin.nvim;
+
+  toLuaValue =
+    value:
+    if builtins.isAttrs value then
+      toLuaTable value
+    else if lib.isString value || lib.isPath value then
+      ''"${toString value}"''
+    else if lib.isList value then
+      "{${lib.strings.concatStringsSep "," (map toLuaValue value)}}"
+    else if lib.isBool value then
+      if value == true then "true" else "false"
+    else
+      toString value;
+
+  toLuaTable =
+    attrset:
+    let
+      values = lib.strings.concatStringsSep "," (
+        lib.attrsets.mapAttrsToList (name: value: "${name} = ${toLuaValue value}") attrset
+      );
+    in
+    "{${values}}";
 in
 
 {
-  options.catppuccin.nvim = catppuccinLib.mkCatppuccinOption { name = "neovim"; };
+  options.catppuccin.nvim = catppuccinLib.mkCatppuccinOption { name = "neovim"; } // {
+    extraConfig = lib.mkOption {
+      description = "Extra settings that will be passed to the setup function.";
+      default = { };
+      type = lib.types.attrs;
+    };
+  };
 
   imports = catppuccinLib.mkRenamedCatppuccinOptions {
     from = [
@@ -32,10 +60,18 @@ in
               vim.fn.mkdir(compile_path, "p")
               vim.opt.runtimepath:append(compile_path)
 
-              require("catppuccin").setup({
-              	compile_path = compile_path,
-              	flavour = "${cfg.flavor}",
-              })
+              local catppuccin_custom_plugin_settings = ${toLuaTable cfg.extraConfig}
+
+              local catppuccin_plugin_settings = {
+                compile_path = compile_path,
+                flavour = "${cfg.flavor}",
+              }
+
+              for key, value in pairs(catppuccin_custom_plugin_settings) do
+                catppuccin_plugin_settings[key] = value
+              end
+
+              require("catppuccin").setup(catppuccin_plugin_settings)
 
               vim.api.nvim_command("colorscheme catppuccin")
             EOF
