30924f53f942d009d08e8a493ca6465977fb6f49
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-03-03 21:55:52 +0100

    feat(home-manager): add support for thunderbird
    fix #349
    
    Co-authored-by: isabel <isabel@isabelroses.com>

---
+++ modules/home-manager/thunderbird.nix
@@ -0,0 +1,50 @@
+{ catppuccinLib }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  inherit (config.catppuccin) sources;
+  cfg = config.catppuccin.thunderbird;
+  enable = cfg.enable && config.programs.thunderbird.enable;
+in
+
+{
+  options.catppuccin.thunderbird =
+    catppuccinLib.mkCatppuccinOption {
+      name = "thunderbird";
+      accentSupport = true;
+    }
+    // {
+      profile = lib.mkOption {
+        type = lib.types.str;
+        default = "catppuccin-${cfg.flavor}-${cfg.accent}";
+        description = "The profile name";
+      };
+    };
+
+  config = lib.mkIf enable {
+    programs.thunderbird = {
+      profiles."${cfg.profile}".extensions = [
+        (pkgs.runCommandLocal "catppuccin-${cfg.flavor}-${cfg.accent}.thunderbird.theme"
+          {
+            buildInputs = [ sources.thunderbird ];
+            nativeBuildInputs = with pkgs; [
+              jq
+              unzip
+            ];
+          }
+          ''
+            xpi=${sources.thunderbird}/${cfg.flavor}/${cfg.flavor}-${cfg.accent}.xpi
+            ext_id=$(unzip -qc $xpi manifest.json | jq -r .applications.gecko.id)
+            firefox_id={ec8030f7-c20a-464f-9b0e-13a3a9e97384}
+            install -Dv $xpi $out/share/mozilla/extensions/$firefox_id/$ext_id.xpi
+          ''
+        )
+      ];
+    };
+  };
+}
