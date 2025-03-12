557e52a8f9deed4c9286d41d0ab9bb6a4d0803ea
isabel <isabel@isabelroses.com>
2025-03-12 20:45:39 +0000

    fix(pkgs/rofi): remove import at build

--- pkgs/rofi/package.nix
+++ pkgs/rofi/package.nix
@@ -3,6 +3,15 @@
 buildCatppuccinPort {
   port = "rofi";
 
+  buildPhase = ''
+    runHook preBuild
+
+    # remove the @import line that is included in the file by default
+    sed -i '1,2d' catppuccin-default.rasi
+
+    runHook postBuild
+  '';
+
   installTargets = [
     "themes"
     "catppuccin-default.rasi"
