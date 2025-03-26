d289659c77ed15571d16bc993ee1416c9601017c
isabel <isabel@isabelroses.com>
2025-03-26 20:02:11 +0000

    feat(pkgs): build vscode with support for overrides

--- pkgs/vscode/package.nix
+++ pkgs/vscode/package.nix
@@ -1,10 +1,95 @@
-{ vscode-utils, ... }:
+{
+  vscode-utils,
+  sources,
+  nodejs_22,
+  pnpm,
 
-vscode-utils.buildVscodeMarketplaceExtension {
-  mktplcRef = {
-    name = "catppuccin-vsc";
-    publisher = "catppuccin";
-    version = "3.16.1";
-    hash = "sha256-qEwQ583DW17dlJbODN8SNUMbDMCR1gUH4REaFkQT65I=";
+  accentColor ? null,
+  accent ? "mauve",
+  boldKeywords ? true,
+  italicComments ? true,
+  italicKeywords ? true,
+  extraBordersEnabled ? false,
+  workbenchMode ? "default",
+  bracketMode ? "rainbow",
+  colorOverrides ? { },
+  customUIColors ? { },
+}:
+
+let
+
+  pname = "catppuccin-vscode";
+  version = builtins.substring 0 7 src.rev;
+  src = sources.vscode;
+
+  pnpmWorkspaces = [ "catppuccin-vsc" ];
+
+  extention = vscode-utils.buildVscodeExtension {
+    name = pname;
+    inherit
+      pname
+      version
+      src
+      ;
+    vscodeExtPublisher = "catppuccin";
+    vscodeExtName = "vscode";
+    vscodeExtUniqueId = "catppuccin.vscode";
+
+    inherit pnpmWorkspaces;
+    pnpmDeps = pnpm.fetchDeps {
+      inherit
+        pname
+        version
+        src
+        pnpmWorkspaces
+        ;
+      hash = "sha256-W3Ztsy7M4ub0Se2o3ZOe/EMWeXXC+D9Mph0t/UYFTXY=";
+    };
+
+    nativeBuildInputs = [
+      nodejs_22
+      (pnpm.override { nodejs = nodejs_22; }).configHook
+    ];
+
+    env.CATPPUCCIN_OPTIONS = builtins.toJSON {
+      inherit
+        accentColor
+        accent
+        boldKeywords
+        italicComments
+        italicKeywords
+        extraBordersEnabled
+        workbenchMode
+        bracketMode
+        colorOverrides
+        customUIColors
+        ;
+    };
+
+    buildPhase = ''
+      runHook preBuild
+
+      pnpm --filter catppuccin-vsc core:build
+
+      cd packages/catppuccin-vsc
+      node dist/hooks/generateThemes.cjs
+      touch ./themes/.flag
+
+      runHook postBuild
+    '';
   };
-}
+
+in
+
+extention.overrideAttrs (_: {
+  sourceRoot = null;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p "$out/$installPrefix"
+    cp -rL ../../LICENSE ../../README.md package.json icon.png dist/ themes/ "$out/$installPrefix/"
+
+    runHook postInstall
+  '';
+})
