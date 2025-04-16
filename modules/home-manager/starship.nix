e74dd68f20f2f292e382d020668852b9f7b03ee2
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-04-16 11:00:10 +0700

    Update starship.nix to match starship.toml
    this one: https://github.com/catppuccin/starship/blob/main/starship.toml

--- modules/home-manager/starship.nix
+++ modules/home-manager/starship.nix
@@ -3,12 +3,12 @@
 
 let
   inherit (config.catppuccin) sources;
-
   cfg = config.catppuccin.starship;
 in
-
 {
-  options.catppuccin.starship = catppuccinLib.mkCatppuccinOption { name = "starship"; };
+  options.catppuccin.starship = catppuccinLib.mkCatppuccinOption {
+    name = "starship";
+  };
 
   imports = catppuccinLib.mkRenamedCatppuccinOptions {
     from = [
@@ -25,6 +25,21 @@ in
         format = lib.mkDefault "$all";
         palette = "catppuccin_${cfg.flavor}";
       } // lib.importTOML "${sources.starship}/${cfg.flavor}.toml";
+
+      character = {
+        success_symbol = "[[󰄛](green) ❯](peach)";
+        error_symbol = "[[󰄛](red) ❯](peach)";
+        vimcmd_symbol = "[󰄛 ❮](subtext1)";
+      };
+
+      git_branch = {
+        style = "bold mauve";
+      };
+
+      directory = {
+        truncation_length = 4;
+        style = "bold lavender";
+      };
     };
   };
 }
