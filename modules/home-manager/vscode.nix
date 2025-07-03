d2ef208ce57590858fdf8f4c50ef8228feb3fd5f
Different <hello@different-name.dev>
2025-07-04 09:29:33 +1000

    feat(home-manager/vscode): Add support for per-profile config

--- modules/home-manager/vscode.nix
+++ modules/home-manager/vscode.nix
@@ -5,68 +5,93 @@
   pkgs,
   ...
 }:
-
 let
+  inherit (lib) types;
   inherit (config.catppuccin) sources;
   cfg = config.catppuccin.vscode;
 
-  usesPerProfileCfg = lib.versionAtLeast catppuccinLib.getModuleRelease "25.05";
-  settings = {
-    extensions = [ (sources.vscode.override { catppuccinOptions = cfg.settings; }) ];
-
-    userSettings = {
-      "workbench.colorTheme" = "Catppuccin " + (catppuccinLib.mkFlavorName cfg.flavor);
-      "catppuccin.accentColor" = cfg.accent;
-
-      # Recommended settings
-      # https://github.com/catppuccin/vscode?tab=readme-ov-file#vscode-settings
-      "editor.semanticHighlighting.enabled" = lib.mkDefault true;
-      "terminal.integrated.minimumContrastRatio" = lib.mkDefault 1;
-      "window.titleBarStyle" = lib.mkDefault "custom";
-    };
-  };
-
   settingsFormat = pkgs.formats.json { };
 
-  settingsSubmodule = {
-    freeformType = settingsFormat.type;
-
-    options = {
-      accent = lib.mkOption {
-        type = catppuccinLib.types.accent;
-        default = cfg.accent;
-      };
-    };
-  };
+  renamedOptions = [
+    "accent"
+    "enable"
+    "flavor"
+    "settings"
+  ];
+  ctpVscOptionPath = [
+    "catppuccin"
+    "vscode"
+  ];
+  newOptionPath = ctpVscOptionPath ++ [
+    "profiles"
+    "default"
+  ];
 in
-
 {
-  options.catppuccin.vscode =
-    catppuccinLib.mkCatppuccinOption {
-      name = "vscode";
-      accentSupport = true;
-    }
-    // {
-      settings = lib.mkOption {
-        description = ''
-          Settings for the extension theme.
+  # Rename old catppuccin.vscode.* options to catppuccin.vscode.profiles.<profile>.*
+  imports = map (
+    option: lib.mkRenamedOptionModule (ctpVscOptionPath ++ [ option ]) (newOptionPath ++ [ option ])
+  ) renamedOptions;
 
-          See https://github.com/catppuccin/vscode/blob/8ac8c5e1db78174c98c55ecd9c1bd3a6f2cbbc0b/packages/catppuccin-vsc/src/theme/index.ts#L14-L25 for a full list of options.
-        '';
-        default = { };
-        type = lib.types.submodule settingsSubmodule;
-      };
+  options.catppuccin.vscode.profiles = lib.mkOption {
+    type = types.attrsOf (
+      types.submodule (
+        { config, ... }:
+        {
+          options =
+            catppuccinLib.mkCatppuccinOption {
+              name = "vscode";
+              accentSupport = true;
+            }
+            // {
+              settings = lib.mkOption {
+                description = ''
+                  Settings for the extension theme.
+
+                  See https://github.com/catppuccin/vscode/blob/8ac8c5e1db78174c98c55ecd9c1bd3a6f2cbbc0b/packages/catppuccin-vsc/src/theme/index.ts#L14-L25 for a full list of options.
+                '';
+
+                default = { };
+
+                type = types.submodule {
+                  freeformType = settingsFormat.type;
+
+                  options = {
+                    accent = lib.mkOption {
+                      type = catppuccinLib.types.accent;
+                      default = config.accent;
+                    };
+                  };
+                };
+              };
+            };
+        }
+      )
+    );
+
+    # by default we list the `default` profile to enable the theme with just the `.enable` option
+    default = {
+      default = { };
     };
 
-  config = lib.mkIf cfg.enable {
-    # TODO: Remove compat layer when 25.05 is stable
-    # https://github.com/nix-community/home-manager/pull/5640
-    programs.vscode =
-      if usesPerProfileCfg then
-        {
-          profiles.default = settings;
-        }
-      else
-        settings;
+    description = "Catppuccin settings for VSCode profiles.";
   };
+
+  config.programs.vscode.profiles = lib.mapAttrs (
+    _: profile:
+    lib.mkIf profile.enable {
+      extensions = [ (sources.vscode.override { catppuccinOptions = profile.settings; }) ];
+
+      userSettings = {
+        "workbench.colorTheme" = "Catppuccin " + (catppuccinLib.mkFlavorName profile.flavor);
+        "catppuccin.accentColor" = profile.accent;
+
+        # Recommended settings
+        # https://github.com/catppuccin/vscode?tab=readme-ov-file#vscode-settings
+        "editor.semanticHighlighting.enabled" = lib.mkDefault true;
+        "terminal.integrated.minimumContrastRatio" = lib.mkDefault 1;
+        "window.titleBarStyle" = lib.mkDefault "custom";
+      };
+    }
+  ) cfg.profiles;
 }
