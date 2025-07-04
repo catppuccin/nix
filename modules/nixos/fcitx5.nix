c4c3df651082217710617a631d826a3c3d1ba075
isabel <isabel@isabelroses.com>
2025-07-04 14:46:43 +0100

    feat(fctix5): add enableRounded option (#600)

--- modules/nixos/fcitx5.nix
+++ modules/nixos/fcitx5.nix
@@ -6,14 +6,19 @@
 }:
 
 let
+  inherit (config.catppuccin) sources;
   cfg = config.catppuccin.fcitx5;
 in
 
 {
-  options.catppuccin.fcitx5 = catppuccinLib.mkCatppuccinOption {
-    name = "Fcitx5";
-    accentSupport = true;
-  };
+  options.catppuccin.fcitx5 =
+    catppuccinLib.mkCatppuccinOption {
+      name = "Fcitx5";
+      accentSupport = true;
+    }
+    // {
+      enableRounded = lib.mkEnableOption "rounded corners for the Fcitx5 theme";
+    };
 
   imports = catppuccinLib.mkRenamedCatppuccinOptions {
     from = [
@@ -28,7 +33,9 @@ in
 
   config = lib.mkIf cfg.enable {
     i18n.inputMethod.fcitx5 = {
-      addons = [ config.catppuccin.sources.fcitx5 ];
+      addons = [
+        (sources.fcitx5.override { inherit (cfg) enableRounded; })
+      ];
       settings.addons.classicui.globalSection.Theme = "catppuccin-${cfg.flavor}-${cfg.accent}";
     };
   };
