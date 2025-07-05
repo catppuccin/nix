719bb50ca2c99bc9c077669a48bfd9815493a11d
Alejandro Angulo <5242883+alejandro-angulo@users.noreply.github.com>
2025-07-05 01:51:56 -0700

    chore(lazygit): remove IFD (#604)

--- modules/home-manager/lazygit.nix
+++ modules/home-manager/lazygit.nix
@@ -1,9 +1,27 @@
 { catppuccinLib }:
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   inherit (config.catppuccin) sources;
+
   cfg = config.catppuccin.lazygit;
+  enable = cfg.enable && config.programs.lazygit.enable;
+
+  # NOTE: On MacOS specifically, lazygit expects its configuration to be in
+  # `~/Library/Application Support` when not using XDG
+  enableXdgConfig = !pkgs.stdenv.hostPlatform.isDarwin || config.xdg.enable;
+
+  configDirectory =
+    if enableXdgConfig then
+      config.xdg.configHome
+    else
+      "${config.home.homeDirectory}/Library/Application Support";
+  configFile = "${configDirectory}/lazygit/config.yml";
 in
 
 {
@@ -22,10 +40,16 @@ in
     accentSupport = true;
   };
 
-  config = lib.mkIf cfg.enable {
-    # TODO: Find a way to "source" a config file with $LG_CONFIG_FILE
-    # and *not* come across https://github.com/catppuccin/nix/issues/455
-    # https://github.com/jesseduffield/lazygit/blob/4b4d82e13c568f3b43c018740132454446cb0a2e/docs/Config.md#overriding-default-config-file-location
-    programs.lazygit.settings = catppuccinLib.importYAML (sources.lazygit + "/${cfg.flavor}/${cfg.accent}.yml");
+  config = lib.mkIf enable {
+    home.sessionVariables =
+      let
+        configFiles = [
+          "${sources.lazygit}/${cfg.flavor}/${cfg.accent}.yml"
+        ] ++ lib.optional (config.programs.lazygit.settings != { }) configFile;
+      in
+      {
+        # Ensure that the default config file is still sourced
+        LG_CONFIG_FILE = lib.concatStringsSep "," configFiles;
+      };
   };
 }
