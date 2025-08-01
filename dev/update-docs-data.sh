932ad0d7ad44fdabc4c7ef0d1a3895ab581cec73
Seth Flynn <getchoo@tuta.io>
2025-08-01 18:58:58 -0400

    ci: start tracking 25.05 docs (#640)
    (cherry picked from commit 4023222ac21fa758870b59c4156d8a4c9fba231e)

--- dev/update-docs-data.sh
+++ dev/update-docs-data.sh
@@ -1,21 +1,35 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+root="$(dirname "$0")/.."
+
 log() {
   echo "=> $*"
 }
 
-update_options() {
-  readonly version_name="$1"
-  shift
-  for type in "nixos" "home"; do
-    nix build \
-      --no-link --print-out-paths \
-      "./dev#${type}OptionsJSON" \
-      "$@" |
-      xargs -I {} cp {}/share/doc/nixos/options.json docs/data/"$version_name"-"$type"-options.json
-    log "generated '$type' options for version '$version_name'"
-  done
+usage() {
+  echo "Usage: $0 <version_name> [flake_ref] [<nix_build_arguments>...]"
 }
 
-update_options main
+if [[ $# -lt 1 ]]; then
+  usage
+  exit 1
+fi
+
+readonly version_name="$1"
+shift
+if [[ -n ${1:-} ]]; then
+  readonly flake_ref="$1"
+  shift
+else
+  readonly flake_ref="$root/dev"
+fi
+
+for type in "nixos" "home"; do
+  nix build \
+    --no-link --print-out-paths \
+    "$flake_ref#${type}OptionsJSON" \
+    "$@" |
+    xargs -I {} cp {}/share/doc/nixos/options.json "$root"/docs/data/"$version_name"-"$type"-options.json
+  log "generated '$type' options for version '$version_name'"
+done
