77280621d5f03b73f144fd74d497e7bf66779311
Seth Flynn <getchoo@tuta.io>
2025-08-01 18:31:37 -0400

    chore!(home-manager/gtk): remove (#644)
    BREAKING CHANGE: This module has long been deprecated, and now appears
    to be quite broken in some cases. I think it's time to let it go

--- modules/home-manager/gtk.nix
+++ modules/home-manager/gtk.nix
@@ -9,71 +9,23 @@
 let
   inherit (lib)
     concatStringsSep
-    mkEnableOption
-    mkIf
-    mkMerge
-    mkOption
     mkRenamedOptionModule
-    types
+    mkRemovedOptionModule
+    toList
     ;
 
   cfg = config.catppuccin.gtk;
-  enable = cfg.enable && config.gtk.enable;
-in
 
-{
-  options.catppuccin.gtk =
-    catppuccinLib.mkCatppuccinOption {
-      name = "gtk";
-      useGlobalEnable = false;
-
-      accentSupport = true;
-    }
-    // {
-      size = mkOption {
-        type = types.enum [
-          "standard"
-          "compact"
-        ];
-        default = "standard";
-        description = "Catppuccin size variant for gtk";
-      };
-
-      tweaks = mkOption {
-        type = types.listOf (
-          types.enum [
-            "black"
-            "rimless"
-            "normal"
-            "float"
-          ]
-        );
-        default = [ ];
-        description = "Catppuccin tweaks for gtk";
-      };
-
-      gnomeShellTheme = mkEnableOption "Catppuccin gtk theme for GNOME Shell";
-
-      icon = catppuccinLib.mkCatppuccinOption {
-        name = "GTK modified Papirus icon theme";
-        # NOTE: we exclude this from the global `catppuccin.enable` as there is no
-        # `enable` option in the upstream module to guard it
-        default = false;
-
-        accentSupport = true;
-      };
-    };
-
-  imports =
-    (catppuccinLib.mkRenamedCatppuccinOptions {
+  namespaceRenameModules =
+    catppuccinLib.mkRenamedCatppuccinOptions {
       from = [
         "gtk"
         "catppuccin"
       ];
       to = "gtk";
       accentSupport = true;
-    })
-    ++ (catppuccinLib.mkRenamedCatppuccinOptions {
+    }
+    ++ catppuccinLib.mkRenamedCatppuccinOptions {
       from = [
         "gtk"
         "catppuccin"
@@ -81,7 +33,7 @@ in
       ];
       to = "cursors";
       accentSupport = true;
-    })
+    }
     ++ [
       (mkRenamedOptionModule
         [
@@ -136,78 +88,57 @@ in
       )
     ];
 
-  config = mkMerge [
-    (mkIf (enable || cfg.gnomeShellTheme) {
-      warnings = [
-        ''
-          `gtk.catppuccin.enable` and `gtk.catppuccin.gnomeShellTheme` are deprecated and will be removed in a future release.
+  # Relative to `catppuccin.gtk`
+  removedOptions = [
+    "enable"
+    "flavor"
+    "accent"
 
-          The upstream port has been archived and support will no longer be provided.
-          Please see https://github.com/catppuccin/gtk/issues/262
-        ''
-      ];
-    })
-
-    (mkIf enable {
-      gtk.theme =
-        let
-          gtkTweaks = concatStringsSep "," cfg.tweaks;
-        in
-        {
-          name =
-            "catppuccin-${cfg.flavor}-${cfg.accent}-${cfg.size}+"
-            + (if (cfg.tweaks == [ ]) then "default" else gtkTweaks);
-          package = config.catppuccin.sources.gtk.override {
-            inherit (cfg) flavor size tweaks;
-            accents = [ cfg.accent ];
-          };
-        };
-
-      xdg.configFile =
-        let
-          gtk4Dir = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0";
-        in
-        {
-          "gtk-4.0/assets".source = "${gtk4Dir}/assets";
-          "gtk-4.0/gtk.css".source = "${gtk4Dir}/gtk.css";
-          "gtk-4.0/gtk-dark.css".source = "${gtk4Dir}/gtk-dark.css";
-        };
-    })
-
-    (mkIf cfg.icon.enable {
-      gtk.iconTheme =
-        let
-          # use the light icon theme for latte
-          polarity = if cfg.icon.flavor == "latte" then "Light" else "Dark";
-        in
-        {
-          name = "Papirus-${polarity}";
-          package = pkgs.catppuccin-papirus-folders.override { inherit (cfg.icon) accent flavor; };
-        };
-    })
-
-    (mkIf cfg.gnomeShellTheme {
-      assertions = [
-        {
-          assertion = enable;
-          message = "`gtk.enable` and `gtk.catppuccin.enable` must be `true` to use the GNOME shell theme";
-        }
-      ];
-
-      home.packages = [ pkgs.gnomeExtensions.user-themes ];
-
-      dconf.settings = {
-        "org/gnome/shell" = {
-          disable-user-extensions = false;
-          enabled-extensions = [ "user-theme@gnome-shell-extensions.gcampax.github.com" ];
-        };
-        "org/gnome/shell/extensions/user-theme" = {
-          inherit (config.gtk.theme) name;
-        };
-        "org/gnome/desktop/interface" = {
-          color-scheme = if cfg.flavor == "latte" then "default" else "prefer-dark";
-        };
-      };
-    })
+    "gnomeShellTheme"
+    "size"
+    "tweaks"
   ];
+
+  removedOptionModules = map (
+    optionPath:
+
+    let
+      attrPath = [
+        "catppuccin"
+        "gtk"
+      ]
+      ++ toList optionPath;
+      moduleName = concatStringsSep "." attrPath;
+    in
+
+    mkRemovedOptionModule attrPath ''
+      `${moduleName}` was removed from catppuccin/nix, as the upstream port has been archived and began experiencing breakages.
+
+      Please see https://github.com/catppuccin/gtk/issues/262
+    ''
+  ) removedOptions;
+in
+
+{
+  imports = namespaceRenameModules ++ removedOptionModules;
+
+  options.catppuccin.gtk = {
+    icon = catppuccinLib.mkCatppuccinOption {
+      name = "GTK modified Papirus icon theme";
+
+      accentSupport = true;
+    };
+  };
+
+  config = lib.mkIf cfg.icon.enable {
+    gtk.iconTheme =
+      let
+        # use the light icon theme for latte
+        polarity = if cfg.icon.flavor == "latte" then "Light" else "Dark";
+      in
+      {
+        name = "Papirus-${polarity}";
+        package = pkgs.catppuccin-papirus-folders.override { inherit (cfg.icon) accent flavor; };
+      };
+  };
 }
