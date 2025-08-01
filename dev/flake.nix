5f3b3338c8fe1be83cba9aeaf3a14d3eaf0ae1b7
user.email <getchoo@tuta.io>
2025-08-01 16:01:06 -0400

    chore(devflake): pin home-manager to 25.05
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/7035020a507ed616e2b20c61491ae3eaa8e5462c?narHash=sha256-2vlAOJv4lBrE%2BP1uOGhZ1symyjXTRdn/mz0tZ6faQcg%3D' (2025-07-31)
      → 'github:nix-community/home-manager/fc3add429f21450359369af74c2375cb34a2d204?narHash=sha256-oV695RvbAE4%2BR9pcsT9shmp6zE/%2BIZe6evHWX63f2Qg%3D' (2025-07-27)

--- dev/flake.nix
+++ dev/flake.nix
@@ -10,7 +10,7 @@
     flake-utils.url = "github:numtide/flake-utils";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
