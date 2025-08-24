ad015344f592b6ebb82de853b747dd577926ec77
isabel <isabel@isabelroses.com>
2025-08-24 10:34:05 +0100

    chore(vscode-icons): 1.21.0 -> 1.24.0 (#705)

--- pkgs/vscode-icons/package.nix
+++ pkgs/vscode-icons/package.nix
@@ -4,7 +4,7 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     name = "catppuccin-vsc-icons";
     publisher = "catppuccin";
-    version = "1.21.0";
-    hash = "sha256-rWExJ9XJ8nKki8TP0UNLCmslw+aCm1hR2h2xxhnY9bg=";
+    version = "1.24.0";
+    hash = "sha256-2M7N4Ccw9FAaMmG36hGHi6i0i1qR+uPCSgXELAA03Xk=";
   };
 }
