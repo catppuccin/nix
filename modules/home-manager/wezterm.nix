64ce80181cb4dcae698c1dbf3fcd87372c50e668
Signe Rhea Grau Kristensen <hey@sigrau.eu>
2025-09-04 00:48:19 +0200

    feat(wezterm): flavor and accent for plugin options

--- modules/home-manager/wezterm.nix
+++ modules/home-manager/wezterm.nix
@@ -1,24 +1,32 @@
 { catppuccinLib }:
 { config, lib, ... }:
-
 let
   inherit (config.catppuccin) sources;
   cfg = config.catppuccin.wezterm;
 in
 {
-  options.catppuccin.wezterm = catppuccinLib.mkCatppuccinOption { name = "wezterm"; } // {
-    apply = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Apply Catppuccin theme to WezTerm.";
+  options.catppuccin.wezterm =
+    catppuccinLib.mkCatppuccinOption {
+      name = "wezterm";
+      accentSupport = true;
+    }
+    // {
+      apply = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Apply Catppuccin theme to WezTerm.";
+      };
     };
-  };
 
   config = lib.mkIf cfg.enable {
     programs.wezterm = {
       extraConfig = lib.mkBefore (
         ''
           local catppuccin_plugin = "${sources.wezterm}/plugin/init.lua"
+          local catppuccin_config = {
+            flavor = "${cfg.flavor}",
+            accent = "${cfg.accent}",
+          }
         ''
         + lib.optionalString cfg.apply ''
           local config = {}
@@ -26,7 +34,7 @@ in
             config = wezterm.config_builder()
           end
 
-          dofile("${sources.wezterm}/plugin/init.lua").apply_to_config(config)
+          dofile("${sources.wezterm}/plugin/init.lua").apply_to_config(config, catppuccin_config)
         ''
       );
     };
