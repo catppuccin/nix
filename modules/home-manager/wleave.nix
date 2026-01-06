57474f570ab8b6aa8a0de2d3d5a791ad2020b8b6
isabel <isabel@isabelroses.com>
2026-01-06 21:48:35 +0000

    feat(home-manager/wleave): init (#813)
    a exact clone of wlogout but defaulting to the wleave icons. this should
    be fine for now since wleave supports the old wlogout configurations
    style but this may change so its a good idea to have a seprate module.
    catppuccin/wlogout also supports wlogout making this somewhat easy.
    closes https://github.com/catppuccin/nix/issues/740
    
    (cherry picked from commit 6231499132cacf176af9e0951c0460cccc544411)

---
+++ modules/home-manager/wleave.nix
@@ -0,0 +1,66 @@
+{ catppuccinLib }:
+{ config, lib, ... }:
+
+let
+  inherit (config.catppuccin) sources;
+
+  cfg = config.catppuccin.wleave;
+in
+
+{
+  options.catppuccin.wleave =
+    catppuccinLib.mkCatppuccinOption {
+      name = "wleave";
+      accentSupport = true;
+    }
+    // {
+      iconStyle = lib.mkOption {
+        type = lib.types.enum [
+          "wlogout"
+          "wleave"
+        ];
+        description = "Icon style to set in ~/.config/wleave/style.css";
+        default = "wleave";
+        example = lib.literalExpression "wleave";
+      };
+
+      extraStyle = lib.mkOption {
+        type = lib.types.lines;
+        description = "Additional CSS to put in ~/.config/wleave/style.css";
+        default = "";
+        example = lib.literalExpression ''
+          button {
+            border-radius: 2px;
+          }
+
+          #lock {
+            background-image: url("''${config.gtk.iconTheme.package}/share/icons/''${config.gtk.iconTheme.name}/apps/scalable/system-lock-screen.svg");
+          }
+        '';
+      };
+    };
+
+  config = lib.mkIf cfg.enable {
+    programs.wleave.style = lib.concatStrings [
+      ''
+        @import url("${sources.wlogout}/themes/${cfg.flavor}/${cfg.accent}.css");
+      ''
+      (lib.concatMapStrings
+        (icon: ''
+          #${icon} {
+            background-image: url("${sources.wlogout}/icons/${cfg.iconStyle}/${cfg.flavor}/${cfg.accent}/${icon}.svg");
+          }
+        '')
+        [
+          "hibernate"
+          "lock"
+          "logout"
+          "reboot"
+          "shutdown"
+          "suspend"
+        ]
+      )
+      cfg.extraStyle
+    ];
+  };
+}
