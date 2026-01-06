1a93ccb838c6938399573d00a751168a758f8e19
isabel <isabel@isabelroses.com>
2026-01-06 20:21:10 +0000

    feat(pkgs/fetchCatppuccinPort): accept lastModified

--- pkgs/fetchCatppuccinPort/package.nix
+++ pkgs/fetchCatppuccinPort/package.nix
@@ -5,19 +5,25 @@ lib.makeOverridable (
     port,
     rev,
     hash,
+    lastModified ? null,
     ...
   }@args:
 
   let
-    arguments = [ "port" ];
+    arguments = [
+      "port"
+      "lastModified"
+    ];
   in
 
   fetchFromGitHub (
-    {
+    lib.recursiveUpdate {
       owner = "catppuccin";
       repo = port;
       inherit rev hash;
-    }
-    // lib.removeAttrs args arguments
+      passthru = lib.optionalAttrs (lastModified != null) {
+        lastModified = lastModified;
+      };
+    } (lib.removeAttrs args arguments)
   )
 )
