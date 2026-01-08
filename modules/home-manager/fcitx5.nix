catppuccin/nix

commit fc247adb17ffd0b76d67b2da12a9e3fe059ccd29

fcitx5: set home-manager theme via addons (#797)

modules/home-manager/fcitx5.nix
@@ -15,10 +15,6 @@ let
       )
       || config.i18n.inputMethod.enabled == "fcitx5"
     );
-
-  package = sources.fcitx5.override {
-    inherit (cfg) enableRounded;
-  };
 in
 
 {
@@ -69,19 +65,11 @@ in
     ];
 
   config = lib.mkIf enable {
-    xdg.dataFile = {
-      "fcitx5/themes/catppuccin-${cfg.flavor}-${cfg.accent}" = {
-        source = "${package}/share/fcitx5/themes/catppuccin-${cfg.flavor}-${cfg.accent}";
-        recursive = true;
-      };
-    };
-
-    xdg.configFile = {
-      "fcitx5/conf/classicui.conf" = lib.mkIf cfg.apply {
-        text = lib.generators.toINIWithGlobalSection { } {
-          globalSection.Theme = "catppuccin-${cfg.flavor}-${cfg.accent}";
-        };
-      };
+    i18n.inputMethod.fcitx5 = {
+      addons = [
+        (sources.fcitx5.override { inherit (cfg) enableRounded; })
+      ];
+      settings.addons.classicui.globalSection.Theme = "catppuccin-${cfg.flavor}-${cfg.accent}";
     };
   };
 }

commit 637655fc835035a9427dfe5efaee5e96a4747231

fcitx5: respect catppuccin.fcitx5.apply
Bug introduced in #797 1c72be4

modules/home-manager/fcitx5.nix
@@ -41,7 +41,9 @@ in
       addons = [
         (sources.fcitx5.override { inherit (cfg) enableRounded; })
       ];
-      settings.addons.classicui.globalSection.Theme = "catppuccin-${cfg.flavor}-${cfg.accent}";
+      settings.addons = lib.mkIf cfg.apply {
+        classicui.globalSection.Theme = "catppuccin-${cfg.flavor}-${cfg.accent}";
+      };
     };
   };
 }