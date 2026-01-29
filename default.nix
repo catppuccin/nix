f670364edcb0f1df2923b38d646794dd9034538a
LuoChen <luochen1990@gmail.com>
2026-01-29 14:03:24 +0800

    fix(nixos): rename deprecated displayManager.environment

--- default.nix
+++ default.nix
@@ -37,19 +37,20 @@ in
 
 {
   # Filter out derivations not available on/meant for the current system
-  packages = lib.filterAttrs (lib.const (
-    deriv:
-    let
-      # Only export packages available on the current system, *unless* they are being cross compiled
-      availableOnHost = lib.meta.availableOn pkgs.stdenv.hostPlatform deriv;
-      # `nix flake check` doesn't like broken packages
-      broken = deriv.meta.broken or false;
-      isCross = deriv.stdenv.buildPlatform != deriv.stdenv.targetPlatform;
-      # Make sure we don't remove our functions
-      isFunction = lib.isFunction deriv;
-    in
-    isFunction || (!broken) && availableOnHost || isCross
-  )) catppuccinPackages;
+  packages = lib.filterAttrs (
+    name: deriv:
+    if name == "sources" || lib.isFunction deriv then
+      false
+    else
+      let
+        # Only export packages available on the current system, *unless* they are being cross compiled
+        availableOnHost = lib.meta.availableOn pkgs.stdenv.hostPlatform deriv;
+        # `nix flake check` doesn't like broken packages
+        broken = deriv.meta.broken or false;
+        isCross = deriv.stdenv.buildPlatform != deriv.stdenv.targetPlatform;
+      in
+      (!broken) && availableOnHost || isCross
+  ) catppuccinPackages;
 
   shell = import ./shell.nix { inherit pkgs; };
 }
