2577f3e742a71030fadbe9f53b1a48764ad40572
isabel <isabel@isabelroses.com>
2026-04-24 10:47:46 +0100

    feat(site): add types
    end my suffering here please

--- docs/data/options.ts
+++ docs/data/options.ts
@@ -2,7 +2,24 @@ import { readFileSync } from "node:fs";
 
 const branches = ["main", "25.11", "25.05", "v1.1", "v1.2"];
 
-const groupOptionsByModule = (options: Object) => {
+export interface OptionData {
+  declarations: string[];
+  default: { _type: string; text: string };
+  description: string;
+  example?: { _type: string; text: string };
+  loc: string[];
+  readOnly: boolean;
+  type: string;
+}
+
+export type OptionEntries = [string, OptionData][];
+export type OptionsByModule = Record<string, OptionEntries>;
+export type OptionsByType = Record<string, OptionsByModule>;
+export type OptionsData = Record<string, OptionsByType>;
+
+const groupOptionsByModule = (
+  options: Record<string, OptionData>,
+): OptionsByModule => {
   return Object.groupBy(Object.entries(options), ([optionName, _]) => {
     const optionNameParts = optionName.split(".");
     // Make the (somewhat naive) assumption that modules are either at the root namespace, or one level below
@@ -10,18 +27,20 @@ const groupOptionsByModule = (options: Object) => {
     // ex: `catppuccin.enable` => `catppuccin`
     const cutoff = Math.min(2, optionNameParts.length - 1);
     return optionNameParts.splice(0, cutoff).join(".");
-  });
+  }) as OptionsByModule;
 };
 
-const loadOptionsFromJSON = (version: string, type: string): Object => {
+const loadOptionsFromJSON = (
+  version: string,
+  type: string,
+): OptionsByModule => {
   const data = JSON.parse(
     readFileSync(`./docs/data/${version}-${type}-options.json`, "utf-8"),
   );
-  const options = groupOptionsByModule(data);
-  return options;
+  return groupOptionsByModule(data);
 };
 
-export const optionsData = Object.fromEntries(
+export const optionsData: OptionsData = Object.fromEntries(
   branches.map((version) => [
     version,
     {
