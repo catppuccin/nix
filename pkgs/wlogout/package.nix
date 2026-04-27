01498c865f11abfe2f3d4e980302a40b23b0fd2c
isabel <isabel@isabelroses.com>
2026-04-28 00:58:04 +0100

    fix(wlogout): build

--- pkgs/wlogout/package.nix
+++ pkgs/wlogout/package.nix
@@ -3,6 +3,22 @@
 buildCatppuccinPort {
   port = "wlogout";
 
+  whiskersTemplates = [
+    "templates/wlogout.tera"
+    "templates/icons/wleave/hibernate.tera"
+    "templates/icons/wleave/lock.tera"
+    "templates/icons/wleave/logout.tera"
+    "templates/icons/wleave/reboot.tera"
+    "templates/icons/wleave/shutdown.tera"
+    "templates/icons/wleave/suspend.tera"
+    "templates/icons/wlogout/hibernate.tera"
+    "templates/icons/wlogout/lock.tera"
+    "templates/icons/wlogout/logout.tera"
+    "templates/icons/wlogout/reboot.tera"
+    "templates/icons/wlogout/shutdown.tera"
+    "templates/icons/wlogout/suspend.tera"
+  ];
+
   installTargets = [
     "themes"
     "icons"
