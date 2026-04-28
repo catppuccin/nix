6cd65b9af5600a1528c61d972c76ffa4d5bdc2f9
isabel <isabel@isabelroses.com>
2026-04-28 01:13:33 +0100

    chore(whiskers): modernize

--- pkgs/whiskers/package.nix
+++ pkgs/whiskers/package.nix
@@ -5,17 +5,19 @@
   rustPlatform,
 }:
 
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "whiskers";
   version = "2.9.0";
 
   src = fetchCatppuccinPort {
     port = "whiskers";
-    rev = "refs/tags/v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-KU2cHBtz9rdfhulINRaQm+YZ7n8OBULrSHSSxmoitnk=";
   };
 
   cargoHash = "sha256-40IPDdxKTWYxsCfsECsXDGwfxXiTEIelxIGAFv3xlU4=";
+
+  __structuredAttrs = true;
 
   passthru = {
     updateScript = nix-update-script { };
@@ -25,5 +27,10 @@ rustPlatform.buildRustPackage rec {
     description = "Soothing port creation tool for the high-spirited!";
     homepage = "https://catppuccin.com";
     license = lib.licenses.mit;
+    mainProgram = "whiskers";
+    maintainers = with lib.maintainers; [
+      getchoo
+      isabelroses
+    ];
   };
-}
+})
