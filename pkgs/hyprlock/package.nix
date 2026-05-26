96bf46de3e35bc8e4e1040db43d57f21ae8648e2
isabel <isabel@isabelroses.com>
2026-05-27 00:19:49 +0100

    fix(hyprlock): handle pin hyprland.conf files (#951)

--- pkgs/hyprlock/package.nix
+++ pkgs/hyprlock/package.nix
@@ -1,7 +1,10 @@
-{ buildCatppuccinPort }:
+{ buildCatppuccinPort, fetchCatppuccinPort }:
 
-buildCatppuccinPort {
+buildCatppuccinPort (finalAttrs: {
   port = "hyprlock";
+
+  dontCatppuccinBuild = true;
+  dontCatppuccinInstall = true;
 
   # Make sure we aren't sourcing possibly non-existent files
   # or overriding our own settings
@@ -9,6 +12,19 @@ buildCatppuccinPort {
     sed -i '1,4d' hyprlock.conf
   '';
 
-  dontCatppuccinBuild = true;
-  installTargets = [ "hyprlock.conf" ];
-}
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out
+    mv hyprlock.conf $out/
+    cp ${finalAttrs.passthru.hyprland}/themes/* $out/
+
+    runHook postInstall
+  '';
+
+  passthru.hyprland = fetchCatppuccinPort {
+    port = "hyprland";
+    tag = "v1.3";
+    hash = "sha256-jkk021LLjCLpWOaInzO4Klg6UOR4Sh5IcKdUxIn7Dis=";
+  };
+})
