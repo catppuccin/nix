b63208eff16cc237b5ddbdf7ebf0f98e7ae48c25
isabel <isabel@isabelroses.com>
2026-05-27 20:10:38 +0100

    chore: set nixpkgs to stable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Soothing pastel theme for Nix";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-26.05/nixexprs.tar.xz";
   };
 
   outputs =
