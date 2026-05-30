22b36c36ebe70a1d8f6d02bae743c7fa415c5c7f
isabel <isabel@isabelroses.com>
2026-05-30 16:20:00 +0100

    feat!(home-manager/gtk): drop removal warning (#965)

--- modules/home-manager/gtk.nix
+++ modules/home-manager/gtk.nix
@@ -7,48 +7,10 @@
 }:
 
 let
-  inherit (lib)
-    concatStringsSep
-    mkRemovedOptionModule
-    toList
-    ;
-
   cfg = config.catppuccin.gtk;
-
-  # Relative to `catppuccin.gtk`
-  removedOptions = [
-    "enable"
-    "flavor"
-    "accent"
-
-    "gnomeShellTheme"
-    "size"
-    "tweaks"
-  ];
-
-  removedOptionModules = map (
-    optionPath:
-
-    let
-      attrPath = [
-        "catppuccin"
-        "gtk"
-      ]
-      ++ toList optionPath;
-      moduleName = concatStringsSep "." attrPath;
-    in
-
-    mkRemovedOptionModule attrPath ''
-      `${moduleName}` was removed from catppuccin/nix, as the upstream port has been archived and began experiencing breakages.
-
-      Please see https://github.com/catppuccin/gtk/issues/262
-    ''
-  ) removedOptions;
 in
 
 {
-  imports = removedOptionModules;
-
   options.catppuccin.gtk = {
     icon = catppuccinLib.mkCatppuccinOption {
       name = "GTK modified Papirus icon theme";
