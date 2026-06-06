714602a642f6369dfab55c87baa861732f665dd4
isabel <isabel@isabelroses.com>
2026-06-06 22:33:02 +0100

    feat(home-manager/fzf): use fzfrc file
    depends on https://github.com/catppuccin/fzf/pull/30. removes IFD

--- modules/home-manager/fzf.nix
+++ modules/home-manager/fzf.nix
@@ -1,42 +1,35 @@
 { catppuccinLib }:
-{ config, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 let
   inherit (config.catppuccin) sources;
 
   cfg = config.catppuccin.fzf;
-  palette = (lib.importJSON "${sources.palette}/palette.json").${cfg.flavor}.colors;
-
-  # Manually populate with colors from catppuccin/fzf
-  # The ordering is meant to match the order of catppuccin/fzf to make comparison easier
-  colors = lib.attrsets.mapAttrs (_: color: palette.${color}.hex) {
-    "bg+" = "surface0";
-    bg = "base";
-    spinner = "rosewater";
-    hl = cfg.accent;
-    fg = "text";
-    header = cfg.accent;
-    info = cfg.accent;
-    pointer = cfg.accent;
-    marker = cfg.accent;
-    "fg+" = "text";
-    prompt = cfg.accent;
-    "hl+" = cfg.accent;
-    selected-bg = "surface1";
-    border = "overlay0";
-    label = "text";
-  };
+  enable = config.catppuccin.enable && cfg.enable && config.programs.fzf.enable;
 in
 
 {
-  options.catppuccin.fzf = catppuccinLib.mkCatppuccinOption {
-    name = "fzf";
-    accentSupport = true;
-  };
+  imports = [
+    (lib.mkRemovedOptionModule [ "catppuccin" "fzf" "accent" ] ''
+      in order to remove IFD from the fzf port we have started to use the
+      FZF_DEFAULT_OPTS_FILE env var. and the upstream catppuccin port does not
+      currently support accents.
 
-  config = lib.mkIf (config.catppuccin.enable && cfg.enable) {
-    programs.fzf = {
-      inherit colors;
+      if you wish to keep using a accent consider using `programs.fzf.colors`
+      to set the colors yourself.
+    '')
+  ];
+
+  options.catppuccin.fzf = catppuccinLib.mkCatppuccinOption { name = "fzf"; };
+
+  config = lib.mkIf enable {
+    home.sessionVariables = {
+      FZF_DEFAULT_OPTS_FILE = "${sources.fzf}/catppuccin-fzf-${cfg.flavor}.rc";
     };
   };
 }
